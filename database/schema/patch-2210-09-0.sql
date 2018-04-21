a9d728f00fd2952d031d31f4737bf075a9e4b3ac
Colin Watson <cjwatson@canonical.com>
2018-04-21 11:53:17 +0100

    Add ArchiveFile.date_created and ArchiveFile.date_superseded
    LP: #1765933

---
+++ database/schema/patch-2210-09-0.sql
@@ -0,0 +1,13 @@
+-- Copyright 2018 Canonical Ltd.  This software is licensed under the
+-- GNU Affero General Public License version 3 (see the file LICENSE).
+
+SET client_min_messages=ERROR;
+
+ALTER TABLE ArchiveFile
+    ADD COLUMN date_created timestamp without time zone,
+    ADD COLUMN date_superseded timestamp without time zone;
+
+COMMENT ON COLUMN ArchiveFile.date_created IS 'The date when this file was created.';
+COMMENT ON COLUMN ArchiveFile.date_superseded IS 'The date when this file was removed or superseded by a newer version.';
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2210, 09, 0);
