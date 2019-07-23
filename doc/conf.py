1fafd6a2a14abe74b72ea0bdaa6785763e09467f
Colin Watson <cjwatson@canonical.com>
2019-07-23 12:04:51 +0100

    Update copyright dates for developer documentation.
    Also bring the copyright holder into sync with that asserted elsewhere in
    the tree.

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Launchpad'
-copyright = u'2011, The Launchpad Developers'
+copyright = u'2004-2019, Canonical Ltd.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
