ce5a526cad01843fd35fae32072a53bf8c22d76d
Colin Watson <cjwatson@canonical.com>
2019-09-24 15:41:39 +0100

    Fix some comment typos.

--- lib/lp/tests/test_no_conflict_marker.py
+++ lib/lp/tests/test_no_conflict_marker.py
@@ -1,7 +1,7 @@
 # Copyright 2009 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-"""Test that no files in the tree has spurious conflicts marker."""
+"""Test that no files in the tree has spurious conflicts markers."""
 
 __metaclass__ = type
 
@@ -11,24 +11,24 @@ import unittest
 
 
 class NoSpuriousConflictsMarkerTest(unittest.TestCase):
-    """Check each file in the working tree for spurious conflicts marker."""
+    """Check each file in the working tree for spurious conflicts markers."""
 
     # We do not check for ======= because it might match some
     # old heading style in some doctests.
     CONFLICT_MARKER_RE = r'^\(<<<<<<< TREE\|>>>>>>> MERGE-SOURCE\)$'
 
     # We could use bzrlib.workingtree for that test, but this cause
-    # problems when the system bzr (so the developers' branch) isn't at
+    # problems when the system bzr (so the developer's branch) isn't at
     # the same level than the bzrlib included in our tree. Anyway, it's
-    # probably faster to use grep anyway.
+    # probably faster to use grep.
     def test_noSpuriousConflictsMarker(self):
-        """Fail if any spurious conflicts marker are found."""
+        """Fail if any spurious conflicts markers are found."""
         root_dir = os.path.join(os.path.dirname(__file__), '../../..')
         shell_command = "bzr ls -R --versioned | xargs grep '%s'" % (
             self.CONFLICT_MARKER_RE)
 
-        # We need to reset PYTHONPATH here otherwise the bzrlib in our
-        # tree will be picked up.
+        # We need to reset PYTHONPATH here, otherwise the bzr in our tree
+        # will be picked up.
         new_env = dict(os.environ)
         new_env['PYTHONPATH'] = ''
         process = subprocess.Popen(
