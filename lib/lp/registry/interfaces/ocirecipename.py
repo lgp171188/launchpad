6acf77401a4a7d4f6692cac75e3de504989cb005
Tom Wardill <tom@howrandom.net>
2019-10-09 15:17:57 +0000

    More standard from the standard template

--- lib/lp/registry/interfaces/ocirecipename.py
+++ lib/lp/registry/interfaces/ocirecipename.py
@@ -1,7 +1,9 @@
 # Copyright 2019 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-from __future__ import unicode_literals
+"""OCI Recipe Name interfaces."""
+
+from __future__ import absolute_import, print_function, unicode_literals
 
 __metaclass__ = type
 __all__ = [
--- lib/lp/registry/model/ocirecipename.py
+++ lib/lp/registry/model/ocirecipename.py
@@ -1,6 +1,10 @@
 # Copyright 2019 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
+"""OCI Recipe Name implementation."""
+
+from __future__ import absolute_import, print_function, unicode_literals
+
 __metaclass__ = type
 __all__ = [
     'OCIRecipeName',
