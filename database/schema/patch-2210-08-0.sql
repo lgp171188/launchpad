6b1a8afc84355a848d7c78bb40dbb5b9a5cdc697
Colin Watson <cjwatson@canonical.com>
2019-10-11 13:46:30 +0100

    Rename OCIRecipeTarget to OCIProject
    Other *Target names in Launchpad are interfaces rather than structural
    elements, so OCIRecipeTarget wasn't a great choice.
    
    We haven't been able to think of a perfect name, but OCIProject isn't
    too bad: it conveys the notion of this being a container, and this does
    share some characteristics with (sub)projects.  The only major downside
    is that it's going to look a little odd when we have OCIProjects within
    projects (in the pillar sense), but that's probably tolerable.
    
    Similarly, rename OCIRecipeName to OCIProjectName, and
    OCIRecipeTargetSeries to OCIProjectSeries.

--- database/schema/patch-2210-08-0.sql
+++ database/schema/patch-2210-08-0.sql
@@ -3,59 +3,59 @@
 
 SET client_min_messages=ERROR;
 
-CREATE TABLE OCIRecipeName (
+CREATE TABLE OCIProjectName (
     id serial PRIMARY KEY,
     name text NOT NULL,
     CONSTRAINT valid_name CHECK (valid_name(name))
 );
 
-COMMENT ON TABLE OCIRecipeName IS 'A name of an Open Container Initiative recipe.';
-COMMENT ON COLUMN OCIRecipeName.name IS 'A lowercase name identifying an OCI recipe.';
+COMMENT ON TABLE OCIProjectName IS 'A name of an Open Container Initiative project.';
+COMMENT ON COLUMN OCIProjectName.name IS 'A lowercase name identifying an OCI project.';
 
-CREATE UNIQUE INDEX ocirecipename__name__key ON OCIRecipeName (name);
-CREATE INDEX ocirecipename__name__trgm ON OCIRecipeName
+CREATE UNIQUE INDEX ociprojectname__name__key ON OCIProjectName (name);
+CREATE INDEX ociprojectname__name__trgm ON OCIProjectName
     USING gin (name trgm.gin_trgm_ops);
 
-CREATE TABLE OCIRecipeTarget (
+CREATE TABLE OCIProject (
     id serial PRIMARY KEY,
     date_created timestamp without time zone DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL,
     date_last_modified timestamp without time zone DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL,
     registrant integer NOT NULL REFERENCES person,
     distribution integer NOT NULL REFERENCES distribution,
-    ocirecipename integer NOT NULL REFERENCES ocirecipename,
+    ociprojectname integer NOT NULL REFERENCES ociprojectname,
     description text,
     bug_reporting_guidelines text,
     bug_reported_acknowledgement text,
     enable_bugfiling_duplicate_search boolean DEFAULT true NOT NULL
 );
 
-COMMENT ON TABLE OCIRecipeTarget IS 'A target (pillar and name) for Open Container Initiative recipes.';
-COMMENT ON COLUMN OCIRecipeTarget.date_created IS 'The date on which this target was created in Launchpad.';
-COMMENT ON COLUMN OCIRecipeTarget.date_last_modified IS 'The date on which this target was last modified in Launchpad.';
-COMMENT ON COLUMN OCIRecipeTarget.registrant IS 'The user who registered this target.';
-COMMENT ON COLUMN OCIRecipeTarget.distribution IS 'The distribution that this target belongs to.';
-COMMENT ON COLUMN OCIRecipeTarget.ocirecipename IS 'The name of this target.';
-COMMENT ON COLUMN OCIRecipeTarget.description IS 'A short description of this target.';
-COMMENT ON COLUMN OCIRecipeTarget.bug_reporting_guidelines IS 'Guidelines to the end user for reporting bugs on this target';
-COMMENT ON COLUMN OCIRecipeTarget.bug_reported_acknowledgement IS 'A message of acknowledgement to display to a bug reporter after they''ve reported a new bug.';
-COMMENT ON COLUMN OCIRecipeTarget.enable_bugfiling_duplicate_search IS 'Enable/disable a search for possible duplicates when a bug is filed.';
+COMMENT ON TABLE OCIProject IS 'A project containing Open Container Initiative recipes.';
+COMMENT ON COLUMN OCIProject.date_created IS 'The date on which this OCI project was created in Launchpad.';
+COMMENT ON COLUMN OCIProject.date_last_modified IS 'The date on which this OCI project was last modified in Launchpad.';
+COMMENT ON COLUMN OCIProject.registrant IS 'The user who registered this OCI project.';
+COMMENT ON COLUMN OCIProject.distribution IS 'The distribution that this OCI project belongs to.';
+COMMENT ON COLUMN OCIProject.ociprojectname IS 'The name of this OCI project.';
+COMMENT ON COLUMN OCIProject.description IS 'A short description of this OCI project.';
+COMMENT ON COLUMN OCIProject.bug_reporting_guidelines IS 'Guidelines to the end user for reporting bugs on this OCI project';
+COMMENT ON COLUMN OCIProject.bug_reported_acknowledgement IS 'A message of acknowledgement to display to a bug reporter after they''ve reported a new bug.';
+COMMENT ON COLUMN OCIProject.enable_bugfiling_duplicate_search IS 'Enable/disable a search for possible duplicates when a bug is filed.';
 
-CREATE UNIQUE INDEX ocirecipetarget__distribution__ocirecipename__key
-    ON OCIRecipeTarget (distribution, ocirecipename)
+CREATE UNIQUE INDEX ociproject__distribution__ociprojectname__key
+    ON OCIProject (distribution, ociprojectname)
     WHERE distribution IS NOT NULL;
 
-CREATE TABLE OCIRecipeTargetSeries (
+CREATE TABLE OCIProjectSeries (
     id serial PRIMARY KEY,
-    ocirecipetarget integer NOT NULL REFERENCES ocirecipetarget,
+    ociproject integer NOT NULL REFERENCES ociproject,
     name text NOT NULL,
     CONSTRAINT valid_name CHECK (valid_name(name))
 );
 
-COMMENT ON TABLE OCIRecipeTargetSeries IS 'A series of an Open Container Initiative recipe target, used to allow tracking bugs against multiple versions of images.';
-COMMENT ON COLUMN OCIRecipeTargetSeries.ocirecipetarget IS 'The target that this series belongs to.';
-COMMENT ON COLUMN OCIRecipeTargetSeries.name IS 'The name of this series.';
+COMMENT ON TABLE OCIProjectSeries IS 'A series of an Open Container Initiative project, used to allow tracking bugs against multiple versions of images.';
+COMMENT ON COLUMN OCIProjectSeries.ociproject IS 'The OCI project that this series belongs to.';
+COMMENT ON COLUMN OCIProjectSeries.name IS 'The name of this series.';
 
-CREATE UNIQUE INDEX ocirecipetargetseries__ocirecipetarget__name__key
-    ON OCIRecipeTargetSeries (ocirecipetarget, name);
+CREATE UNIQUE INDEX ociprojectseries__ociproject__name__key
+    ON OCIProjectSeries (ociproject, name);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2210, 08, 0);
