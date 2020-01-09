lgp171188/launchpad

commit e6db69425adec2ce77d67d210b6bf1cead298bb9

Oops should report request's person.name instead of auth framework's getLogin()

lib/lp/services/webapp/errorlog.py
@@ -177,7 +177,9 @@ def attach_http_request(report, context):
 
     missing = object()
     principal = getattr(request, 'principal', missing)
-    if safe_hasattr(principal, 'getLogin'):
+    if safe_hasattr(principal, 'person'):
+        login = principal.person.name
+    elif safe_hasattr(principal, 'getLogin'):
         login = principal.getLogin()
     elif principal is missing or principal is None:
         # Request has no principal (e.g. scriptrequest)

lib/lp/services/webapp/tests/test_errorlog.py
@@ -11,6 +11,7 @@ from textwrap import dedent
 import traceback
 
 from fixtures import TempDir
+import mock
 from lazr.batchnavigator.interfaces import InvalidBatchSizeError
 from lazr.restful.declarations import error_status
 import oops_amqp
@@ -166,6 +167,35 @@ class TestErrorReportingUtility(TestCase):
         # verify that the oopsid was set on the request
         self.assertEqual(request.oopsid, report['id'])
         self.assertEqual(request.oops, report)
+
+    def test_raising_request_with_principal_person(self):
+        utility = ErrorReportingUtility()
+        utility._main_publishers[0].__call__ = lambda report: []
+
+        request = TestRequestWithPrincipal(
+            environ={
+                'SERVER_URL': 'http://localhost:9000/foo',
+                'HTTP_COOKIE': 'lp=cookies_hidden_for_security_reasons',
+                'name1': 'value1',
+            },
+            form={
+                'name1': 'value3 \xa7',
+                'name2': 'value2',
+                u'\N{BLACK SQUARE}': u'value4',
+            })
+
+        request.setInWSGIEnvironment('launchpad.pageid', 'IFoo:+foo-template')
+        # Set a fake person attached to the request.
+        # Report should use his name (instead of principal.getLogin)
+        request.principal.person = mock.Mock()
+        request.principal.person.name = 'my_username'
+
+        try:
+            raise ArbitraryException('xyz\nabc')
+        except ArbitraryException:
+            report = utility.raising(sys.exc_info(), request)
+        self.assertEqual(u'my_username, 42, title, description |\u25a0|',
+                         report['username'])
 
     def test_raising_with_xmlrpc_request(self):
         # Test ErrorReportingUtility.raising() with an XML-RPC request.
@@ -545,15 +575,19 @@ class TestRequestWithUnauthenticatedPrincipal(TestRequest):
 
 
 class TestRequestWithPrincipal(TestRequest):
+    def __init__(self, *args, **kw):
+        super(TestRequestWithPrincipal, self).__init__(*args, **kw)
+        self._principal = TestRequestWithPrincipal.Principal()
 
     def setInWSGIEnvironment(self, key, value):
         self._orig_env[key] = value
 
-    class principal:
-        id = 42
-        title = u'title'
-        # non ASCII description
-        description = u'description |\N{BLACK SQUARE}|'
+    class Principal:
+        def __init__(self):
+            self.id = 42
+            self.title = u'title'
+            # non ASCII description
+            self.description = u'description |\N{BLACK SQUARE}|'
 
         @staticmethod
         def getLogin():

commit c332d4bc8bbc9310fe8ff32a5dfd0c42c4f9e403

Cache Person.name for attach_http_request
Commit e6db69425adec2ce77d67d210b6bf1cead298bb9 introduced a regression:
if the request timed out, then the attempt to fetch
principal.person.name in attach_http_request could itself raise
RequestExpired if the relevant property wasn't already cached, causing
the OOPS to be lost.

Instead, we cache Person.name on LaunchpadPrincipal earlier in the
request, allowing us to use it safely while building an OOPS report.

lib/lp/services/webapp/authentication.py
@@ -264,9 +264,14 @@ class LaunchpadPrincipal:
         self.scope_url = scope_url
         self.account = account
         self.person = IPerson(account, None)
+        self.person_name = (
+            self.person.name if self.person is not None else None)
 
     def getLogin(self):
-        return self.title
+        if self.person_name is not None:
+            return self.person_name
+        else:
+            return self.title
 
 
 def get_oauth_authorization(request):

lib/lp/services/webapp/errorlog.py
@@ -26,7 +26,6 @@ from zope.event import notify
 from zope.exceptions.exceptionformatter import format_exception
 from zope.interface import implementer
 from zope.publisher.interfaces.xmlrpc import IXMLRPCRequest
-from zope.security.proxy import removeSecurityProxy
 from zope.traversing.namespace import view
 
 from lp.app import versioninfo
@@ -41,7 +40,6 @@ from lp.services.webapp.adapter import (
 from lp.services.webapp.interfaces import (
     IErrorReportEvent,
     IErrorReportRequest,
-    ILaunchpadPrincipal,
     IUnloggedException,
     )
 from lp.services.webapp.opstats import OpStats
@@ -180,13 +178,7 @@ def attach_http_request(report, context):
     missing = object()
     principal = getattr(request, 'principal', missing)
 
-    person = (
-        removeSecurityProxy(principal.person)
-        if ILaunchpadPrincipal.providedBy(principal)
-        else None)
-    if person is not None:
-        login = person.name
-    elif safe_hasattr(principal, 'getLogin'):
+    if safe_hasattr(principal, 'getLogin'):
         login = principal.getLogin()
     elif principal is missing or principal is None:
         # Request has no principal (e.g. scriptrequest)

lib/lp/services/webapp/interfaces.py
@@ -593,6 +593,9 @@ class ILaunchpadPrincipal(IPrincipal):
 
     person = Attribute("The IPerson the principal represents.")
 
+    person_name = Attribute(
+        "The name of the IPerson the principal represents.")
+
 
 #
 # Browser notifications

lib/lp/services/webapp/tests/test_errorlog.py
@@ -30,6 +30,7 @@ from lp.app.errors import (
     )
 from lp.layers import WebServiceLayer
 from lp.services.config import config
+from lp.services.database.sqlbase import flush_database_caches
 from lp.services.webapp.authentication import LaunchpadPrincipal
 from lp.services.webapp.errorlog import (
     _filter_session_statement,
@@ -174,10 +175,14 @@ class TestErrorReportingUtility(TestCaseWithFactory):
         person = self.factory.makePerson(name='my-username')
         request = TestRequestWithPrincipal(account=person.account)
 
+        # Make sure Storm would have to reload person.name if it were used.
+        flush_database_caches()
+
         try:
             raise ArbitraryException('xyz\nabc')
         except ArbitraryException:
-            report = utility.raising(sys.exc_info(), request)
+            report = self.assertStatementCount(
+                0, utility.raising, sys.exc_info(), request)
         self.assertEqual(
             u'my-username, 42, account-name, description |\u25a0|',
             report['username'])