8a20656859612e8be133d523f0c17fe1b18fef4d
Colin Watson <cjwatson@canonical.com>
2020-01-14 23:31:22 +0000

    Stop recreating GitRepository indexes here
    These should be done in a follow-up hot patch instead.  As preparation,
    rename the old indexes out of the way.

--- database/schema/patch-2210-08-1.sql
+++ database/schema/patch-2210-08-1.sql
@@ -1,4 +1,4 @@
--- Copyright 2019 Canonical Ltd.  This software is licensed under the
+-- Copyright 2019-2020 Canonical Ltd.  This software is licensed under the
 -- GNU Affero General Public License version 3 (see the file LICENSE).
 
 SET client_min_messages=ERROR;
@@ -18,75 +18,22 @@ ALTER TABLE GitRepository
 
 COMMENT ON COLUMN GitRepository.ociprojectname IS 'The OCI project that this repository belongs to.';
 
--- Create/replace indexes.  Previously we had a variety of indexes including
--- (distribution, sourcepackagename) with non-NULL distribution but omitting
--- the condition that sourcepackagename is also non-NULL.  Replace each of
--- these with a pair of indexes, one for non-NULL sourcepackagename and one
--- for non-NULL ociprojectname.
+-- Rename some indexes in preparation for replacing them with versions that
+-- include the condition that sourcepackagename is non-NULL.
 
-DROP INDEX gitrepository__owner__distribution__sourcepackagename__name__key;
-CREATE UNIQUE INDEX gitrepository__owner__distribution__spn__name__key
-    ON GitRepository (owner, distribution, sourcepackagename, name)
-    WHERE distribution IS NOT NULL AND sourcepackagename IS NOT NULL;
-CREATE UNIQUE INDEX gitrepository__owner__distribution__ocipn__name__key
-    ON GitRepository (owner, distribution, ociprojectname, name)
-    WHERE distribution IS NOT NULL AND ociprojectname IS NOT NULL;
-
-DROP INDEX gitrepository__distribution__spn__target_default__key;
-CREATE UNIQUE INDEX gitrepository__distribution__spn__target_default__key
-    ON GitRepository (distribution, sourcepackagename)
-    WHERE
-        distribution IS NOT NULL AND sourcepackagename IS NOT NULL
-        AND target_default;
-CREATE UNIQUE INDEX gitrepository__distribution__ocipn__target_default__key
-    ON GitRepository (distribution, ociprojectname)
-    WHERE
-        distribution IS NOT NULL AND ociprojectname IS NOT NULL
-        AND target_default;
-
-DROP INDEX gitrepository__owner__distribution__spn__owner_default__key;
-CREATE UNIQUE INDEX gitrepository__owner__distribution__spn__owner_default__key
-    ON GitRepository (owner, distribution, sourcepackagename)
-    WHERE
-        distribution IS NOT NULL AND sourcepackagename IS NOT NULL
-        AND owner_default;
-CREATE UNIQUE INDEX gitrepository__owner__distribution__ocipn__owner_default__key
-    ON GitRepository (owner, distribution, ociprojectname)
-    WHERE
-        distribution IS NOT NULL AND ociprojectname IS NOT NULL
-        AND owner_default;
-
-DROP INDEX gitrepository__distribution__spn__date_last_modified__idx;
-CREATE INDEX gitrepository__distribution__spn__date_last_modified__idx
-    ON GitRepository (distribution, sourcepackagename, date_last_modified)
-    WHERE distribution IS NOT NULL AND sourcepackagename IS NOT NULL;
-CREATE INDEX gitrepository__distribution__ocipn__date_last_modified__idx
-    ON GitRepository (distribution, ociprojectname, date_last_modified)
-    WHERE distribution IS NOT NULL AND ociprojectname IS NOT NULL;
-
-DROP INDEX gitrepository__distribution__spn__id__idx;
-CREATE INDEX gitrepository__distribution__spn__id__idx
-    ON GitRepository (distribution, sourcepackagename, id)
-    WHERE distribution IS NOT NULL AND sourcepackagename IS NOT NULL;
-CREATE INDEX gitrepository__distribution__ocipn__id__idx
-    ON GitRepository (distribution, ociprojectname, id)
-    WHERE distribution IS NOT NULL AND ociprojectname IS NOT NULL;
-
-DROP INDEX gitrepository__owner__distribution__spn__date_last_modified__idx;
-CREATE INDEX gitrepository__owner__distribution__spn__date_last_modified__idx
-    ON GitRepository (
-        owner, distribution, sourcepackagename, date_last_modified)
-    WHERE distribution IS NOT NULL AND sourcepackagename IS NOT NULL;
-CREATE INDEX gitrepository__owner__distribution__ocipn__date_last_modified__idx
-    ON GitRepository (owner, distribution, ociprojectname, date_last_modified)
-    WHERE distribution IS NOT NULL AND ociprojectname IS NOT NULL;
-
-DROP INDEX gitrepository__owner__distribution__spn__id__idx;
-CREATE INDEX gitrepository__owner__distribution__spn__id__idx
-    ON GitRepository (owner, distribution, sourcepackagename, id)
-    WHERE distribution IS NOT NULL AND sourcepackagename IS NOT NULL;
-CREATE INDEX gitrepository__owner__distribution__ocipn__id__idx
-    ON GitRepository (owner, distribution, ociprojectname, id)
-    WHERE distribution IS NOT NULL AND ociprojectname IS NOT NULL;
+ALTER INDEX gitrepository__owner__distribution__sourcepackagename__name__key
+    RENAME TO old__gitrepository__owner__distribution__sourcepackagename__name__key;
+ALTER INDEX gitrepository__distribution__spn__target_default__key
+    RENAME TO old__gitrepository__distribution__spn__target_default__key;
+ALTER INDEX gitrepository__owner__distribution__spn__owner_default__key
+    RENAME TO old__gitrepository__owner__distribution__spn__owner_default__key;
+ALTER INDEX gitrepository__distribution__spn__date_last_modified__idx
+    RENAME TO old__gitrepository__distribution__spn__date_last_modified__idx;
+ALTER INDEX gitrepository__distribution__spn__id__idx
+    RENAME TO old__gitrepository__distribution__spn__id__idx;
+ALTER INDEX gitrepository__owner__distribution__spn__date_last_modified__idx
+    RENAME TO old__gitrepository__owner__distribution__spn__date_last_modified__idx;
+ALTER INDEX gitrepository__owner__distribution__spn__id__idx
+    RENAME TO old__gitrepository__owner__distribution__spn__id__idx;
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2210, 08, 1);
