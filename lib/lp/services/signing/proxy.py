8ec7f01ff2ff4c7f15ff9aa3c82d9c215e0d5e3c
Thiago F Pappacena <thiago.frazao.pappacena@canonical.com>
2020-01-28 13:05:38 -0300

    generate key endpoint with tests

--- lib/lp/services/signing/proxy.py
+++ lib/lp/services/signing/proxy.py
@@ -8,6 +8,7 @@ from __future__ import division
 __metaclass__ = type
 
 import base64
+import json
 
 import requests
 from lp.services.propertycache import cachedproperty
@@ -37,7 +38,7 @@ class SigningService:
         :param path: The REST endpoint to be joined"""
         return self.LP_SIGNING_ADDRESS + path
 
-    def _get_json(self, path, method="GET", payload=None):
+    def _get_json(self, path, method="GET", **kwargs):
         """Helper method to do an HTTP request and get back a json from  the
         signing service, raising exception if status code != 200
         """
@@ -45,7 +46,7 @@ class SigningService:
         if method == "GET":
             response = requests.get(url)
         elif method == "POST":
-            response = requests.post(url, data=payload)
+            response = requests.post(url, **kwargs)
         else:
             raise NotImplemented("Only GET and POST are allowed for now")
 
@@ -54,11 +55,51 @@ class SigningService:
 
     @cachedproperty
     def service_public_key(self):
-        """Returns the lp-signing service's public key"""
-        json = self._get_json("/service-key")
-        return PublicKey(json["service-key"], encoder=Base64Encoder)
+        """Returns the lp-signing service's public key
+        """
+        data = self._get_json("/service-key")
+        return PublicKey(data["service-key"], encoder=Base64Encoder)
+
+    @property
+    def private_key(self):
+        return PrivateKey(self.LOCAL_PRIVATE_KEY, encoder=Base64Encoder)
 
     def get_nonce(self):
-        json = self._get_json("/nonce", "POST")
-        return base64.b64decode(json["nonce"].encode("UTF-8"))
+        """Get nonce, to be used when sending messages
+        """
+        data = self._get_json("/nonce", "POST")
+        return base64.b64decode(data["nonce"].encode("UTF-8"))
+
+    def _get_auth_headers(self, nonce):
+        """Get headers to call authenticated endpoints
+
+        :param nonce: The nonce bytes to be used (not the base64 encoded one!)
+        :return: Header dict, ready to be used by requests
+        """
+        return {
+            "Content-Type": "application/x-boxed-json",
+            "X-Client-Public-Key": self.LOCAL_PUBLIC_KEY,
+            "X-Nonce": base64.b64encode(nonce)}
+
+    def generate(self, key_type, description):
+        """Generate a key to be used when signing
+
+        :param key_type: One of available key types at lp-signing: ["UEFI",
+                         "KMOD", "OPAL", "SIPL", "FIT"]
+        :param description: String description of the generated key
+        :return: A dict with 'fingerprint' (str) and 'public-key' (a
+                Base64-encoded NaCl public key)
+        """
+        if key_type not in {"UEFI", "KMOD", "OPAL", "SIPL", "FIT"}:
+            raise ValueError("%s is not a valid key type" % key_type)
+        nonce = self.get_nonce()
+        data = json.dumps({
+            "key-type": key_type,
+            "description": description,
+            }).encode("UTF-8")
+        box = Box(self.private_key, self.service_public_key)
+        encrypted_message = box.encrypt(data, nonce, encoder=Base64Encoder)
+        return self._get_json(
+            "/generate", "POST", headers=self._get_auth_headers(nonce),
+            data=encrypted_message.ciphertext)
 
--- lib/lp/services/signing/tests/test_proxy.py
+++ lib/lp/services/signing/tests/test_proxy.py
@@ -3,28 +3,177 @@
 
 __metaclass__ = type
 
+import base64
+
 import requests
 from lp.services.signing.proxy import SigningService
 from lp.testing import TestCaseWithFactory
 from lp.testing.layers import ZopelessLayer
 import mock
+from mock import ANY
+from nacl.encoding import Base64Encoder
+from nacl.public import PublicKey
+
+
+class SigningServiceResponseFactory:
+    """Factory for fake responses from lp-signing service
+    """
+    def _get_mock_response(self, status_code, json):
+        mock_response = mock.Mock(requests.Response)
+        mock_response.status_code = status_code
+        mock_response.json.return_value = json
+        return mock_response
+
+    def get_service_key(self, service_key):
+        """Fake response for GET /service-key endpoint
+        """
+        return self._get_mock_response(200, {"service-key": service_key})
+
+    def post_nonce(self, nonce):
+        """Fake response for POST /nonce endpoint
+
+        :param nonce: The base64-encoded nonce, as it would be returned by
+                      the request
+        """
+        return self._get_mock_response(200, {"nonce": nonce})
+
+    def post_generate(self, public_key, finger_print):
+        """Fake response for POST /generate endpoint
+        """
+        return self._get_mock_response(
+            200, {'fingerprint': finger_print, 'public-key': public_key})
+
+    def patch_post_requests(self, requests_module, responses):
+        """Patch the fake "requests module" to return the given responses
+        when certain endpoints are called
+
+        """
+        def side_effect(url, *args, **kwags):
+            for endpoint, response in responses.items():
+                if url.endswith(endpoint):
+                    return response
+            return mock.Mock()
+        requests_module.post.side_effect = side_effect
+        return requests_module
 
 
 @mock.patch("lp.services.signing.proxy.requests")
 class SigningServiceProxyTest(TestCaseWithFactory):
+    """Tests signing service without actually making calls to lp-signing
+    """
     layer = ZopelessLayer
 
+    def setUp(self, *args, **kwargs):
+        super(TestCaseWithFactory, self).setUp(*args, **kwargs)
+        self.response_factory = SigningServiceResponseFactory()
+
     def test_get_service_public_key(self, mock_requests):
-        mock_response = mock.Mock(requests.Response)
-        mock_response.status_code = 200
-        mock_response.json.return_value = {
-            'service-key': 'test-foo'}
+        base64_public_key = "x7vTtpmn0+DvKNdmtf047fn1JRQI5eMnOQRy3xJ1m10="
+        mock_response = self.response_factory.get_service_key(
+            base64_public_key)
         mock_requests.get.return_value = mock_response
 
         signing = SigningService()
-        key = signing.get_service_public_key()
+        key = signing.service_public_key
 
-        self.assertEqual("test-foo", key)
+        # Asserts that the public key is correct.
+        self.assertIsInstance(key, PublicKey)
+        self.assertEqual(key.encode(Base64Encoder), base64_public_key)
+
+        # Asserts that the endpoint was called
         mock_requests.get.assert_called_once_with(
             signing.LP_SIGNING_ADDRESS + "/service-key")
+
+    def test_get_nonce(self, mock_requests):
+        # Server returns base64-encoded nonce, but the
+        # SigningService.get_nonce method returns it already decoded
+        base64_nonce = "neSSa2MUZlQU3XiipU2TfiaqW5nrVUpR"
+        mock_response = self.response_factory.post_nonce(base64_nonce)
+        mock_requests.post.return_value = mock_response
+
+        signing = SigningService()
+        nonce = signing.get_nonce()
+
+        self.assertEqual(base64.b64encode(nonce), base64_nonce)
+
+    def test_generate_unknown_key_type_raises_exception(self, mock_requests):
+        signing = SigningService()
+        self.assertRaises(
+            ValueError, signing.generate, "banana", "Wrong key type")
+        self.assertEqual(0, mock_requests.get.call_count)
+        self.assertEqual(0, mock_requests.post.call_count)
+
+    def test_generate_key(self, mock_requests):
+        """Makes sure that the SigningService.generate method calls the
+        correct endpoints
+
+        This method is actually mocking a lot of things because most of the
+        things are already unit-tested
+        """
+        b64_nonce = "neSSa2MUZlQU3XiipU2TfiaqW5nrVUpR"
+        mock_nonce_response = self.response_factory.post_nonce(b64_nonce)
+
+        public_key = (
+            'LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSURFVENDQWZtZ0F3SUJBZ0l'
+            'VZlgreHlFNUp4VVcyWVBYemVDMGtsQlZZQTBjd0RRWUpLb1pJaHZjTkFRRUwKQl'
+            'FBd0dERVdNQlFHQTFVRUF3d05WR1Z6ZENCclpYa2dWVVZHU1RBZUZ3MHlNREF4T'
+            'WpneE16UTVORGRhRncwegpNREF4TWpVeE16UTVORGRhTUJneEZqQVVCZ05WQkFN'
+            'TURWUmxjM1FnYTJWNUlGVkZSa2t3Z2dFaU1BMEdDU3FHClNJYjNEUUVCQVFVQUE'
+            '0SUJEd0F3Z2dFS0FvSUJBUURPc3Vvd3VGZllRK1g0TjVLZWtXMGxBbmMvemNrYk'
+            '5mUmEKK2hZSE56RmJDa2hMUzZYTWdOY1d5eW8rTk4rd05FN3JEcUgwc3gweUZzc'
+            'zJuVCtxWXM1WFdIYmRXMHBVNnpCTwp0bEh0MjhNQzYzWU03ZkpFZnVpM1RFRXph'
+            'R1VKOUp2dFhENG16Vkd1cGxZczVBckkvc3RvdDVHY0J6bHVuNHJnCkNHNUdtWXR'
+            'KVGw4YkpSWGFqckhMZFJJc2dIWCtXTXBKUCtnQVlnSE94M3Y5VHlXaDJMR0FnbU'
+            'MyYVFSbFE3WEoKS1ZSRzVaYTJVMlRaZ3dnYzc0SkFBNjVIQSt2Z2xtcW5SZExad'
+            '0RRTUluYjZ6djBsL0tDYkFRbldwL2hxMDB6VwoxRkVrR1k2Sm1Jb3BnR0lxTm9E'
+            'WVJOSEllWCtiVHE2eWthcUg5M0pjV2NOYlBZdEZGMUFGQWdNQkFBR2pVekJSCk1'
+            'CMEdBMVVkRGdRV0JCUXFpZXBwdkxOaWZIMjlKV3ByNk4zdmFpYzNDREFmQmdOVk'
+            'hTTUVHREFXZ0JRcWllcHAKdkxOaWZIMjlKV3ByNk4zdmFpYzNDREFQQmdOVkhST'
+            'UJBZjhFQlRBREFRSC9NQTBHQ1NxR1NJYjNEUUVCQ3dVQQpBNElCQVFCYXIzaGk5'
+            'TXFabjJ4eUh5Z3pBQzhGMFlmU2lHNm5aV09jSEJPT1QveVhMOVloTlFxL292TWZ'
+            'TUlJtCnl1SkE3eWlxcUhUU211ZDZ4QWxYZlhQYXpxaUxHSEdpMXl2dWVVOWtmOW'
+            'dHRGp2NW5Kek1YMXBLeUZLcVZjaysKZVdyVEF6S21xL2FTWFRlcFhUNVBoRU14Y'
+            'UdORHlJb3I3ck0rU2JZaWNFZGZoeEZiSTc1UWk3NERCdGlBdmZCbgpsK2JwMk1D'
+            'dDNydS81YiszYUNjRm5Pa0dXQ3I0RW1RZktzSTlYanViblJYNTFVMTVleFdsQW1'
+            'LaVNQRGd4aUcvCm5iSklYVHNUYXNqeVNuaEl0QkFQcWozdlgzRFhFZmlpVm5icH'
+            'dSbEZ4YjhQRlI5cC9hQTExdVp2VXhMYTI1TDQKSEVhdW5sRWdpRnM4Ynl5Z2hTa'
+            'GZJdU40aDZuMwotLS0tLUVORCBDRVJUSUZJQ0FURS0tLS0tCg')
+        fingerprint = '338D218488DFD597D8FCB9C328C3E9D9ADA16CEE'
+        mock_generate_response = self.response_factory.post_generate(
+            public_key, fingerprint)
+
+        # Replace /nonce and /generate responses by our mocks
+        self.response_factory.patch_post_requests(
+            mock_requests, {"/nonce": mock_nonce_response,
+                            "/generate": mock_generate_response})
+
+        # Mock fetching service public key
+        b64_service_pub_key = (
+            "x7vTtpmn0+DvKNdmtf047fn1JRQI5eMnOQRy3xJ1m10=")
+        mock_pub_key_response = self.response_factory.get_service_key(
+            b64_service_pub_key)
+        mock_requests.get.return_value = mock_pub_key_response
+
+        # Generate the key, and checks if we got back the correct dict
+        signing = SigningService()
+        generated = signing.generate("UEFI", "my lp test key")
+
+        self.assertEqual(generated, {
+            'public-key': public_key, 'fingerprint': fingerprint})
+
+        # Asserts it tried to fetch service key, fetched the nonce and posted
+        # to the /generate endpoint
+        mock_requests.get.assert_called_once_with(
+            signing.LP_SIGNING_ADDRESS + "/service-key")
+        mock_nonce_response.json.assert_called_with()
+        mock_generate_response.json.assert_called_with()
+
+        mock_requests.post.assert_called_with(
+            signing.LP_SIGNING_ADDRESS + "/generate",
+            headers={
+                "Content-Type": "application/x-boxed-json",
+                "X-Client-Public-Key": signing.LOCAL_PUBLIC_KEY,
+                "X-Nonce": b64_nonce
+            },
+            data=ANY)  # XXX: check the encrypted data
 
