33f2227f28d78ed900fed527550cf63d60f81ad1
William Grant <william.grant@canonical.com>
2020-02-07 17:55:13 +0000

    Simplify BugSummary triggers further
    This now doesn't use any dynamic queries at all.
    
    [Posted by William to
    https://code.launchpad.net/~cjwatson/launchpad/+git/launchpad/+merge/373765;
    tidied up slightly by Colin.]

--- database/schema/patch-2210-06-0.sql
+++ database/schema/patch-2210-06-0.sql
@@ -13,30 +13,13 @@ DROP TRIGGER bugtaskflat_maintain_bug_summary ON bugtaskflat;
 DROP TRIGGER bugtag_maintain_bug_summary_before_trigger ON bugtag;
 DROP TRIGGER bugtag_maintain_bug_summary_after_trigger ON bugtag;
 
--- Similar to the previous bugsummary_tags, but returns an array of tags on
--- the given bug plus NULL; this can be passed to other functions, and is
--- suitable for constructing a set of rows to insert into BugSummaryJournal
--- when handling changes to BugTaskFlat.
-CREATE OR REPLACE FUNCTION bugsummary_tag_names(bug integer)
-    RETURNS text[]
-    LANGUAGE sql STABLE
-    AS $_$
-    SELECT array_agg(tag)
-    FROM (
-        SELECT BugTag.tag FROM BugTag WHERE BugTag.bug = $1
-        UNION ALL
-        SELECT NULL::text
-    ) AS tag;
-$_$;
-
-COMMENT ON FUNCTION bugsummary_tag_names IS
-    'Return an array of the tag names on the given bug, plus NULL; this is suitable for constructing BugSummaryJournal rows when handling changes to BugTaskFlat.';
-
 -- Modify the existing bugsummary_locations to accept an array of tags
--- rather than looking them up for itself.
+-- rather than looking them up for itself, and return journal rows rather than
+-- bugsummary ones, since we haven't directly written to bugsummary in the
+-- better part of a decade.
 DROP FUNCTION bugsummary_locations;
 CREATE FUNCTION bugsummary_locations(btf_row bugtaskflat, tags text[])
-    RETURNS SETOF bugsummary
+    RETURNS SETOF bugsummaryjournal
     LANGUAGE plpgsql
     AS $$
 BEGIN
@@ -61,62 +44,15 @@ BEGIN
 END;
 $$;
 
--- Modify the existing bugsummary_journal_bugtaskflat to accept an array of
--- tags, and to return a set of rows to the caller rather than inserting
--- them into a temporary table.  This can now be just SQL rather than
--- PL/pgSQL.
-DROP FUNCTION bugsummary_journal_bugtaskflat;
-CREATE FUNCTION bugsummary_journal_bugtaskflat(btf_row bugtaskflat, tags text[], _count integer)
-    RETURNS SETOF bugsummaryjournal
+CREATE OR REPLACE FUNCTION bugsummary_insert_journals(
+        journals bugsummaryjournal[])
+    RETURNS void
     LANGUAGE sql
     AS $$
-    SELECT
-        CAST(NULL AS integer) as id,
-        _count, product, productseries, distribution,
-        distroseries, sourcepackagename, viewed_by, tag,
-        status, milestone, importance, has_patch, access_policy
-        FROM bugsummary_locations(btf_row, tags);
-$$;
-
--- Rewrite the existing bugtaskflat_maintain_bug_summary as a
--- statement-level trigger.
-CREATE OR REPLACE FUNCTION bugtaskflat_maintain_bug_summary()
-    RETURNS trigger
-    LANGUAGE plpgsql SECURITY DEFINER
-    SET search_path TO 'public'
-    AS $$
-DECLARE
-    summary_queries text[] DEFAULT '{}';
-BEGIN
-    -- Work out the subqueries we need to compute the set of
-    -- BugSummaryJournal rows that should be inserted.  (We have to use
-    -- dynamic commands for this, because the transition tables are not
-    -- visible in functions called by this trigger function.)
-    IF TG_OP = 'DELETE' OR TG_OP = 'UPDATE' THEN
-        summary_queries := array_append(summary_queries, $_$
-            SELECT summary.*
-            FROM
-                old_bugtaskflat btf_row,
-                LATERAL bugsummary_journal_bugtaskflat(
-                    btf_row, bugsummary_tag_names(btf_row.bug), -1
-                ) AS summary
-        $_$);
-    END IF;
-    IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' THEN
-        summary_queries := array_append(summary_queries, $_$
-            SELECT summary.*
-            FROM
-                new_bugtaskflat btf_row,
-                LATERAL bugsummary_journal_bugtaskflat(
-                    btf_row, bugsummary_tag_names(btf_row.bug), 1
-                ) AS summary
-        $_$);
-    END IF;
     -- We sum the rows here to minimise the number of inserts into the
     -- journal, as in the case of UPDATE statement we may have -1s and +1s
     -- cancelling each other out.
-    EXECUTE ($_$
-        INSERT INTO BugSummaryJournal(
+    INSERT INTO BugSummaryJournal(
             count, product, productseries, distribution,
             distroseries, sourcepackagename, viewed_by, tag,
             status, milestone, importance, has_patch, access_policy)
@@ -124,15 +60,64 @@ BEGIN
             SUM(count), product, productseries, distribution,
             distroseries, sourcepackagename, viewed_by, tag,
             status, milestone, importance, has_patch, access_policy
-        FROM (
-    $_$ || array_to_string(summary_queries, 'UNION ALL') || $_$
-        ) AS summary
+        FROM unnest(journals)
         GROUP BY
             product, productseries, distribution,
             distroseries, sourcepackagename, viewed_by, tag,
             status, milestone, importance, has_patch, access_policy
         HAVING SUM(count) != 0;
-    $_$);
+$$;
+
+-- Rewrite the existing bugtaskflat_maintain_bug_summary as a
+-- statement-level trigger.
+CREATE TYPE todo_bugsummary_btf_internal AS (
+   btf bugtaskflat,
+   count integer
+);
+
+CREATE OR REPLACE FUNCTION bugtaskflat_maintain_bug_summary()
+    RETURNS trigger
+    LANGUAGE plpgsql SECURITY DEFINER
+    SET search_path TO 'public'
+    AS $$
+DECLARE
+    all_rows todo_bugsummary_btf_internal[];
+    temp_rows todo_bugsummary_btf_internal[];
+    journals bugsummaryjournal[];
+    temp_rec record;
+    temp_journal bugsummaryjournal;
+BEGIN
+    -- Work out the subqueries we need to compute the set of
+    -- BugSummaryJournal rows that should be inserted.
+    IF TG_OP = 'DELETE' OR TG_OP = 'UPDATE' THEN
+        SELECT array_agg(row(old_bugtaskflat, -1))
+            INTO STRICT temp_rows FROM old_bugtaskflat;
+        all_rows := array_cat(all_rows, temp_rows);
+    END IF;
+    IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' THEN
+        SELECT array_agg(row(new_bugtaskflat, 1))
+            INTO STRICT temp_rows FROM new_bugtaskflat;
+        all_rows := array_cat(all_rows, temp_rows);
+    END IF;
+
+    -- XXX wgrant 2020-02-07: "The target is a record variable, row
+    -- variable, or comma-separated list of scalar variables." but a list
+    -- doesn't seem to work.
+    FOR temp_rec IN
+        SELECT journal, row.count
+        FROM
+            unnest(all_rows) AS row,
+            LATERAL bugsummary_locations(
+                row((row.btf).*),
+                (SELECT array_append(array_agg(tag), NULL::text)
+                 FROM bugtag WHERE bug = (row.btf).bug)) AS journal
+    LOOP
+        temp_journal := temp_rec.journal;
+        temp_journal.count := temp_rec.count;
+        journals := array_append(journals, temp_journal);
+    END LOOP;
+
+    PERFORM bugsummary_insert_journals(journals);
     RETURN NULL;
 END;
 $$;
@@ -152,83 +137,62 @@ CREATE TRIGGER bugtaskflat_maintain_bug_summary_delete
     REFERENCING OLD TABLE AS old_bugtaskflat
     FOR EACH STATEMENT EXECUTE PROCEDURE bugtaskflat_maintain_bug_summary();
 
--- Modify the existing bugsummary_journal_bug to accept an array of tags,
--- and to return a set of rows to the caller rather than inserting them into
--- a temporary table.  Rephrasing the loop using LATERAL allows this to be
--- just SQL rather than PL/pgSQL.
-DROP FUNCTION bugsummary_journal_bug;
-CREATE FUNCTION bugsummary_journal_bug(bug_row bug, tags text[], _count integer)
-    RETURNS SETOF bugsummaryjournal
-    LANGUAGE sql
-    AS $$
-    SELECT summary.*
-    FROM
-        bugtaskflat btf_row,
-        LATERAL bugsummary_journal_bugtaskflat(
-            btf_row, tags, _count
-        ) AS summary
-    WHERE bug = bug_row.id;
-$$;
-
 -- Rewrite the existing bugtag_maintain_bug_summary as a statement-level
 -- trigger.
+CREATE TYPE todo_bugsummary_bug_internal AS (
+   bug integer,
+   tags text[],
+   count integer
+);
+
 CREATE OR REPLACE FUNCTION bugtag_maintain_bug_summary()
     RETURNS trigger
     LANGUAGE plpgsql SECURITY DEFINER
     SET search_path TO 'public'
     AS $$
 DECLARE
-    summary_queries text[] DEFAULT '{}';
+    all_rows todo_bugsummary_bug_internal[];
+    temp_rows todo_bugsummary_bug_internal[];
+    journals bugsummaryjournal[];
+    temp_rec record;
+    temp_journal bugsummaryjournal;
 BEGIN
     -- Work out the subqueries we need to compute the set of
-    -- BugSummaryJournal rows that should be inserted.  (We have to use
-    -- dynamic commands for this, because the transition tables are not
-    -- visible in functions called by this trigger function.)
+    -- BugSummaryJournal rows that should be inserted.
     IF TG_OP = 'DELETE' OR TG_OP = 'UPDATE' THEN
-        summary_queries := array_append(summary_queries, $_$
-            SELECT summary.*
-            FROM
-                (SELECT bug, array_agg(tag) AS tags
-                 FROM old_bugtag
-                 GROUP BY bug) AS grouped_bugtags,
-                LATERAL bugsummary_journal_bug(
-                    bug_row(grouped_bugtags.bug), grouped_bugtags.tags, -1
-                ) AS summary
-        $_$);
+        SELECT array_agg(
+            (SELECT row(bug, array_agg(tag), -1) FROM old_bugtag GROUP BY bug))
+            INTO STRICT temp_rows;
+        all_rows := array_cat(all_rows, temp_rows);
     END IF;
     IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' THEN
-        summary_queries := array_append(summary_queries, $_$
-            SELECT summary.*
-            FROM
-                (SELECT bug, array_agg(tag) AS tags
-                 FROM new_bugtag
-                 GROUP BY bug) AS grouped_bugtags,
-                LATERAL bugsummary_journal_bug(
-                    bug_row(grouped_bugtags.bug), grouped_bugtags.tags, 1
-                ) AS summary
-        $_$);
+        SELECT array_agg(
+            (SELECT row(bug, array_agg(tag), 1) FROM new_bugtag GROUP BY bug))
+            INTO STRICT temp_rows;
+        all_rows := array_cat(all_rows, temp_rows);
     END IF;
-    -- We sum the rows here to minimise the number of inserts into the
-    -- journal, as in the case of UPDATE statement we may have -1s and +1s
-    -- cancelling each other out.
-    EXECUTE ($_$
-        INSERT INTO BugSummaryJournal(
-            count, product, productseries, distribution,
-            distroseries, sourcepackagename, viewed_by, tag,
-            status, milestone, importance, has_patch, access_policy)
-        SELECT
-            SUM(count), product, productseries, distribution,
-            distroseries, sourcepackagename, viewed_by, tag,
-            status, milestone, importance, has_patch, access_policy
-        FROM (
-    $_$ || array_to_string(summary_queries, 'UNION ALL') || $_$
-        ) AS summary
-        GROUP BY
-            product, productseries, distribution,
-            distroseries, sourcepackagename, viewed_by, tag,
-            status, milestone, importance, has_patch, access_policy
-        HAVING SUM(count) != 0;
-    $_$);
+
+    -- XXX wgrant 2020-02-07: "The target is a record variable, row
+    -- variable, or comma-separated list of scalar variables." but a list
+    -- doesn't seem to work.
+    FOR temp_rec IN
+        SELECT journal, row.count
+        FROM
+            unnest(all_rows) as row,
+            LATERAL (
+                SELECT journal.*
+                FROM
+                    bugtaskflat btf,
+                    bugsummary_locations(btf, row.tags) AS journal
+                WHERE btf.bug = row.bug
+            ) AS journal
+    LOOP
+        temp_journal := temp_rec.journal;
+        temp_journal.count := temp_rec.count;
+        journals := array_append(journals, temp_journal);
+    END LOOP;
+
+    PERFORM bugsummary_insert_journals(journals);
     RETURN NULL;
 END;
 $$;
@@ -247,5 +211,14 @@ CREATE TRIGGER bugtag_maintain_bug_summary_delete
     AFTER DELETE ON bugtag
     REFERENCING OLD TABLE AS old_bugtag
     FOR EACH STATEMENT EXECUTE PROCEDURE bugtag_maintain_bug_summary();
+
+DROP FUNCTION bugsummary_tags;
+DROP FUNCTION bugsummary_journal_bug;
+DROP FUNCTION bugsummary_journal_bugtaskflat;
+DROP FUNCTION bug_row;
+DROP FUNCTION bug_summary_flush_temp_journal;
+DROP FUNCTION ensure_bugsummary_temp_journal;
+DROP FUNCTION summarise_bug;
+DROP FUNCTION unsummarise_bug;
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2210, 06, 0);
