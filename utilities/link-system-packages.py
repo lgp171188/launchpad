54684b3d58b280e1f2e8c85d61a64be4fbc5510b
Colin Watson <cjwatson@canonical.com>
2020-02-11 18:47:54 +0000

    Stop using --system-site-packages
    We've been using "virtualenv --system-site-packages" to ensure that
    Launchpad has access to some system-installed packages that can't or
    shouldn't be installed as a normal Python dependency for various
    reasons.  This strategy has always had some problems, but we've been
    able to deal with them.
    
    However, with recent versions of pip and Python 3, the situation gets
    worse.  https://github.com/pypa/pip/issues/6264 describes the problem:
    pip attempts to isolate itself from site-packages when building wheels,
    leaving only its temporary one; but within a virtualenv created with
    --system-site-packages it removes the virtualenv's site-packages but not
    the system one (because virtualenv patches
    distutils.sysconfig.get_python_lib).  It's not easy to see how pip might
    reliably and portably escape the virtualenv in order to find the
    system's site-packages directory.
    
    (On Python 2 we get away with this by luck, because
    distutils.sysconfig.get_python_lib returns
    '.../env/lib/python2.7/site-packages' but
    '.../env/local/lib/python2.7/site-packages' is on sys.path, and pip
    doesn't realise that they're the same thing due to symlinks; we don't
    get this stroke of luck on Python 3.)
    
    Installing everything properly in the virtualenv is also difficult.
    Several of the ones that remain don't exist as wheels or in another form
    that pip can install.  We could fake this up (I went so far as to
    prepare a script to convert the installed version of python-apt into a
    wheel), but this would incur a significant maintenance burden.
    
    Instead, symlink packages from the system site-packages directory into
    the virtualenv in "make compile".  It's conceivable that this might
    confuse pip slightly, but so far it doesn't seem to, and everything else
    works fine.

---
+++ utilities/link-system-packages.py
@@ -0,0 +1,47 @@
+#! /usr/bin/python2
+
+# Copyright 2020 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Link system-installed Python modules into Launchpad's virtualenv."""
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from argparse import ArgumentParser
+from distutils.sysconfig import get_python_lib
+import importlib
+import os.path
+import re
+
+
+def link_module(name, virtualenv_libdir):
+    module = importlib.import_module(name)
+    path = module.__file__
+    if os.path.basename(path).startswith("__init__."):
+        path = os.path.dirname(path)
+    system_libdir = get_python_lib(plat_specific=path.endswith(".so"))
+    if os.path.commonprefix([path, system_libdir]) != system_libdir:
+        raise RuntimeError(
+            "%s imported from outside %s (%s)" % (name, system_libdir, path))
+    target_path = os.path.join(
+        virtualenv_libdir, os.path.relpath(path, system_libdir))
+    if os.path.lexists(target_path) and os.path.islink(target_path):
+        os.unlink(target_path)
+    os.symlink(path, target_path)
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("virtualenv_libdir")
+    parser.add_argument("module_file", type=open)
+    args = parser.parse_args()
+
+    for line in args.module_file:
+        line = re.sub(r"#.*", "", line).strip()
+        if not line:
+            continue
+        link_module(line, args.virtualenv_libdir)
+
+
+if __name__ == "__main__":
+    main()
