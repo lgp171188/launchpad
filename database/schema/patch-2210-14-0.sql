d55d3ea98b01000abdc56c905e3ed4e8258d9c00
Thiago F Pappacena <thiago.frazao.pappacena@canonical.com>
2020-03-31 10:37:36 -0300

    Adding index and comment on distribution.oci_project_admin db column

--- database/schema/patch-2210-14-0.sql
+++ database/schema/patch-2210-14-0.sql
@@ -6,4 +6,10 @@ SET client_min_messages=ERROR;
 ALTER TABLE distribution
     ADD COLUMN oci_project_admin integer REFERENCES person;
 
+COMMENT ON COLUMN distribution.oci_project_admin
+    IS 'Person or team with privileges to manage OCI Projects.';
+
+CREATE INDEX distribution__oci_project_admin__idx
+    ON distribution (oci_project_admin);
+
 INSERT INTO LaunchpadDatabaseRevision VALUES (2210, 14, 0);
