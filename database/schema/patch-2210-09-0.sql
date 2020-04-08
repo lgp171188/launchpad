e019f4e898dc201a990db0a33bac06c07ad6f0bc
Colin Watson <cjwatson@canonical.com>
2020-04-08 13:56:09 +0100

    Clarify meaning of ArchiveFile.date_superseded

--- database/schema/patch-2210-09-0.sql
+++ database/schema/patch-2210-09-0.sql
@@ -8,6 +8,6 @@ ALTER TABLE ArchiveFile
     ADD COLUMN date_superseded timestamp without time zone;
 
 COMMENT ON COLUMN ArchiveFile.date_created IS 'The date when this file was created.';
-COMMENT ON COLUMN ArchiveFile.date_superseded IS 'The date when this file was removed or superseded by a newer version.';
+COMMENT ON COLUMN ArchiveFile.date_superseded IS 'The date when this file ceased to hold its path in the archive, due to being removed or superseded by a newer version.';
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2210, 09, 0);
