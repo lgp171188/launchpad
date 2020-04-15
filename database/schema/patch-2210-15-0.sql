4e9689b0c3edaecbb364d49c7521f8b56e02e749
Thiago F Pappacena <thiago.frazao.pappacena@canonical.com>
2020-04-15 14:44:26 -0300

    Adjusting OCIRecipeJob details and adding comments to it

--- database/schema/patch-2210-15-0.sql
+++ database/schema/patch-2210-15-0.sql
@@ -4,14 +4,19 @@
 SET client_min_messages=ERROR;
 
 CREATE TABLE OCIRecipeJob (
-    id serial PRIMARY KEY,
-    job INTEGER NOT NULL REFERENCES Job,
-    oci_recipe INTEGER NOT NULL REFERENCES OCIRecipe,
+    job integer PRIMARY KEY REFERENCES Job ON DELETE CASCADE NOT NULL,
+    recipe INTEGER NOT NULL REFERENCES OCIRecipe,
     job_type INTEGER NOT NULL,
-    json_data TEXT NOT NULL
+    json_data jsonb NOT NULL
 );
 
-CREATE INDEX ocirecipejob__oci_recipe__job_type__job__idx
-  ON OCIRecipeJob(oci_recipe, job_type, job);
+CREATE INDEX ocirecipejob__recipe__job_type__job__idx
+  ON OCIRecipeJob(recipe, job_type, job);
+
+COMMENT ON TABLE OCIRecipeJob IS 'Contains references to jobs that are executed for an OCI Recipe.';
+COMMENT ON COLUMN OCIRecipeJob.job IS 'A reference to a Job row that has all the common job details.';
+COMMENT ON COLUMN OCIRecipeJob.recipe IS 'The OCI recipe that this job is for.';
+COMMENT ON COLUMN OCIRecipeJob.job_type IS 'The type of a job, such as a build request.';
+COMMENT ON COLUMN OCIRecipeJob.json_data IS 'Data that is specific to a particular job type.';
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2210, 15, 0);
