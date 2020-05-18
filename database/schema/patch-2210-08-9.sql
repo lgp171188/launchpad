1e14aeb523a8c257f9a562fd14ff3d606516ddae
Tom Wardill <tom@howrandom.net>
2020-05-18 16:13:50 +0000

    Add OCIFile.date_last_used

---
+++ database/schema/patch-2210-08-9.sql
@@ -0,0 +1,11 @@
+-- Copyright 2020 Canonical Ltd.  This software is licensed under the
+-- GNU Affero General Public License version 3 (see the file LICENSE).
+
+SET client_min_messages=ERROR;
+
+ALTER TABLE OCIFile
+ADD COLUMN date_last_used timestamp without time zone DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL;
+
+COMMENT ON COLUMN OCIFile.date_last_used IS 'The datetime this file was last used in a build.';
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2210, 08, 9);
