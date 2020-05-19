6c133fe88de34341ca3bb4578c7eecfddcce9edc
Tom Wardill <tom@howrandom.net>
2020-05-19 16:18:49 +0000

    Allow edit permissions to oci_project_admin

--- lib/lp/security.py
+++ lib/lp/security.py
@@ -3462,11 +3462,9 @@ class EditOCIProject(AuthorizationBase):
 
     def checkAuthenticated(self, user):
         """Maintainers, drivers, and admins can drive projects."""
-        # XXX twom 2019-10-29 This ideally shouldn't be driver, but a
-        # new role name that cascades upwards from the OCIProject
-        # to the pillar
         return (user.in_admin or
-                user.isDriver(self.obj.pillar))
+                user.isDriver(self.obj.pillar) or
+                user.inTeam(self.obj.pillar.oci_project_admin))
 
 
 class EditOCIProjectSeries(AuthorizationBase):
@@ -3476,7 +3474,8 @@ class EditOCIProjectSeries(AuthorizationBase):
     def checkAuthenticated(self, user):
         """Maintainers, drivers, and admins can drive projects."""
         return (user.in_admin or
-                user.isDriver(self.obj.oci_project.pillar))
+                user.isDriver(self.obj.oci_project.pillar) or
+                user.inTeam(self.obj.oci_project.pillar.oci_project_admin))
 
 
 class ViewOCIRecipeBuildRequest(DelegatedAuthorization):
