6e9ade58db9c4205bc89cd24cdf6a8f4821ac0ba
Tom Wardill <tom@howrandom.net>
2020-05-20 09:36:20 +0000

    add index to date_last_used

--- database/schema/patch-2210-08-9.sql
+++ database/schema/patch-2210-08-9.sql
@@ -8,4 +8,7 @@ ADD COLUMN date_last_used timestamp without time zone DEFAULT (CURRENT_TIMESTAMP
 
 COMMENT ON COLUMN OCIFile.date_last_used IS 'The datetime this file was last used in a build.';
 
+CREATE INDEX ocifile__date_last_used_idx
+    ON OCIFile (date_last_used);
+
 INSERT INTO LaunchpadDatabaseRevision VALUES (2210, 08, 9);
