71bff49ead10fe744282bbdaa2d4fc3475058a8f
Tom Wardill <tom@howrandom.net>
2020-06-02 10:10:54 +0000

    Fix indentation

--- database/schema/patch-2210-16-0.sql
+++ database/schema/patch-2210-16-0.sql
@@ -4,7 +4,8 @@
 SET client_min_messages=ERROR;
 
 ALTER TABLE OCIFile
-ADD COLUMN date_last_used timestamp without time zone DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL;
+ADD COLUMN date_last_used timestamp without time zone
+    DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL;
 
 COMMENT ON COLUMN OCIFile.date_last_used IS 'The datetime this file was last used in a build.';
 
