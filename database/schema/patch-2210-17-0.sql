e600171299c007cb9c6d5df41e43e2d3848467aa
Thiago F Pappacena <thiago.frazao.pappacena@canonical.com>
2020-06-08 19:07:52 -0300

    Adding default value for gitrepo.status

--- database/schema/patch-2210-17-0.sql
+++ database/schema/patch-2210-17-0.sql
@@ -5,7 +5,13 @@ SET client_min_messages=ERROR;
 
 ALTER TABLE GitRepository ADD COLUMN status INTEGER;
 
+-- ALTER COLUMN ... SET DEFAULT doesn't trigger a table rewrite,
+-- while ADD COLUMN ... DEFAULT xx does. In pg <11 this operation is slow.
+-- That's why we first create, and then we set the default value.
+-- Data backfilling will be done in a hot patch instead of a fast downtime.
+ALTER TABLE GitRepository ALTER COLUMN status SET DEFAULT 2;
+
 COMMENT ON COLUMN GitRepository.status
-    IS 'The current situation of this git repository.';
+    IS 'The current status of this git repository.';
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2210, 17, 0);
