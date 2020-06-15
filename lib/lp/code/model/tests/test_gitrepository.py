29db78ad16cf0d3458d6b591a16c51a5cdfe0e5a
Tom Wardill <tom@howrandom.net>
2020-06-15 08:12:51 +0000

    Add test to ensure the ID property is present

--- lib/lp/code/model/tests/test_gitrepository.py
+++ lib/lp/code/model/tests/test_gitrepository.py
@@ -3731,6 +3731,7 @@ class TestGitRepositoryWebservice(TestCaseWithFactory):
         self.assertEqual(201, response.status)
         repository = webservice.get(response.getHeader("Location")).jsonBody()
         self.assertThat(repository, ContainsDict({
+            "id": Equals(1),
             "repository_type": Equals("Hosted"),
             "registrant_link": EndsWith(owner_url),
             "owner_link": EndsWith(owner_url),
