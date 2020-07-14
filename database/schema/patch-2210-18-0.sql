655ad5d2cfc00a484733674461e52e79bac6acbe
Tom Wardill <tom@howrandom.net>
2020-07-14 11:01:12 +0100

    Add allow_internet to OCIRecipe

---
+++ database/schema/patch-2210-18-0.sql
@@ -0,0 +1,10 @@
+-- Copyright 202 Canonical Ltd.  This software is licensed under the
+-- GNU Affero General Public License version 3 (see the file LICENSE).
+
+SET client_min_messages=ERROR;
+
+ALTER TABLE OCIRecipe ADD COLUMN allow_internet boolean DEFAULT true NOT NULL;
+
+COMMENT ON COLUMN OCIRecipe.allow_internet IS 'If True, builds of this OCIRecipe may allow access to external network resources.';
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2210, 18, 0);
