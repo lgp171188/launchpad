c74bb8c6ade8fbd59755508e0222d6117d9584f5
Tom Wardill <tom@howrandom.net>
2020-07-14 11:16:25 +0100

    Correct year

--- database/schema/patch-2210-18-0.sql
+++ database/schema/patch-2210-18-0.sql
@@ -1,4 +1,4 @@
--- Copyright 202 Canonical Ltd.  This software is licensed under the
+-- Copyright 2020 Canonical Ltd.  This software is licensed under the
 -- GNU Affero General Public License version 3 (see the file LICENSE).
 
 SET client_min_messages=ERROR;
