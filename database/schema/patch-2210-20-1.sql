cfa53d652b92a0b14a011364409fd1bad84c2028
Thiago F Pappacena <thiago.frazao.pappacena@canonical.com>
2020-08-24 11:45:59 -0300

    Adding OCIRecipe.build_args column

---
+++ database/schema/patch-2210-20-1.sql
@@ -0,0 +1,10 @@
+-- Copyright 2020 Canonical Ltd.  This software is licensed under the
+-- GNU Affero General Public License version 3 (see the file LICENSE).
+
+SET client_min_messages=ERROR;
+
+ALTER TABLE OCIRecipe ADD COLUMN build_args text DEFAULT '' NOT NULL;
+
+COMMENT ON COLUMN OCIRecipe.build_args IS 'ARGs to be used when building the OCI Recipe.';
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2210, 20, 1);
