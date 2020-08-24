6cb5cda6c2d9a0ffb3c4fe0b5bac35ca9c36ee4b
Thiago F Pappacena <thiago.frazao.pappacena@canonical.com>
2020-08-24 16:52:17 -0300

    Allowing null on OCIRecipe.build_args

--- database/schema/patch-2210-20-1.sql
+++ database/schema/patch-2210-20-1.sql
@@ -3,7 +3,7 @@
 
 SET client_min_messages=ERROR;
 
-ALTER TABLE OCIRecipe ADD COLUMN build_args text DEFAULT '' NOT NULL;
+ALTER TABLE OCIRecipe ADD COLUMN build_args text;
 
 COMMENT ON COLUMN OCIRecipe.build_args IS 'ARGs to be used when building the OCI Recipe.';
 
