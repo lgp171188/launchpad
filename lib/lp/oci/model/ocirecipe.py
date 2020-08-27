7e1096320307b1ba8b609d33a9f8cedffab832bd
Thiago F Pappacena <thiago.frazao.pappacena@canonical.com>
2020-08-27 12:18:01 -0300

    Changing OCIRecipe.build_args datatype to jsonb

--- lib/lp/oci/model/ocirecipe.py
+++ lib/lp/oci/model/ocirecipe.py
@@ -14,6 +14,7 @@ __all__ = [
 
 from lazr.lifecycle.event import ObjectCreatedEvent
 import pytz
+from storm.databases.postgres import JSON
 from storm.expr import (
     And,
     Desc,
@@ -24,7 +25,6 @@ from storm.locals import (
     Bool,
     DateTime,
     Int,
-    JSON,
     Reference,
     Store,
     Storm,
@@ -147,7 +147,7 @@ class OCIRecipe(Storm, WebhookTargetMixin):
     git_path = Unicode(name="git_path", allow_none=True)
     build_file = Unicode(name="build_file", allow_none=False)
 
-    _build_args = JSON(name="build_args", allow_none=False)
+    _build_args = JSON(name="build_args", allow_none=True)
 
     require_virtualized = Bool(name="require_virtualized", default=True,
                                allow_none=False)
@@ -198,8 +198,8 @@ class OCIRecipe(Storm, WebhookTargetMixin):
 
     @build_args.setter
     def build_args(self, value):
-        assert isinstance(value, dict)
-        self._build_args = {k: str(v) for k, v in value.items()}
+        assert value is None or isinstance(value, dict)
+        self._build_args = {k: str(v) for k, v in (value or {}).items()}
 
     def destroySelf(self):
         """See `IOCIRecipe`."""
