ad2aa2d50d4e358bbdca5b4ec6a3f9c17844e4a9
Colin Watson <cjwatson@canonical.com>
2020-10-26 18:21:16 +0000

    Make TestGitRepositoryFork tests stricter

--- lib/lp/code/model/tests/test_gitrepository.py
+++ lib/lp/code/model/tests/test_gitrepository.py
@@ -2748,10 +2748,17 @@ class TestGitRepositoryFork(TestCaseWithFactory):
 
         forked_repo = getUtility(IGitRepositorySet).fork(
             repo, another_person, another_team)
-        self.assertEqual(another_team, forked_repo.owner)
-        self.assertEqual(another_person, forked_repo.registrant)
-        self.assertTrue(forked_repo.owner_default)
-        self.assertEqual(1, self.hosting_fixture.create.call_count)
+        self.assertThat(forked_repo, MatchesStructure(
+            registrant=Equals(another_person),
+            owner=Equals(another_team),
+            target=Equals(repo.target),
+            name=Equals(repo.name),
+            owner_default=Is(True),
+            target_default=Is(False)))
+        self.assertEqual(
+            [((forked_repo.getInternalPath(),),
+              {"clone_from": None, "async_create": True})],
+            self.hosting_fixture.create.calls)
 
     def test_fork_not_owner_default(self):
         repo = self.factory.makeGitRepository()
@@ -2762,23 +2769,35 @@ class TestGitRepositoryFork(TestCaseWithFactory):
 
         forked_repo = getUtility(IGitRepositorySet).fork(
             repo, previous_repo.owner, previous_repo.owner)
-        self.assertEqual(previous_repo.owner, forked_repo.owner)
-        self.assertEqual(previous_repo.owner, forked_repo.registrant)
-        self.assertFalse(forked_repo.owner_default)
+        self.assertThat(forked_repo, MatchesStructure(
+            registrant=Equals(previous_repo.owner),
+            owner=Equals(previous_repo.owner),
+            target=Equals(repo.target),
+            name=Equals(repo.name),
+            owner_default=Is(False),
+            target_default=Is(False)))
         self.assertEqual(1, self.hosting_fixture.create.call_count)
 
     def test_fork_same_name(self):
         repo = self.factory.makeGitRepository()
 
         person = self.factory.makePerson()
-        same_name_repo = self.factory.makeGitRepository(
+        self.factory.makeGitRepository(
             owner=person, registrant=person,
             name=repo.name, target=repo.target)
 
         forked_repo = getUtility(IGitRepositorySet).fork(repo, person, person)
-        self.assertEqual(forked_repo.target, repo.target)
-        self.assertTrue(forked_repo.owner_default)
-        self.assertEqual(forked_repo.name, "%s-1" % same_name_repo.name)
+        self.assertThat(forked_repo, MatchesStructure(
+            registrant=Equals(person),
+            owner=Equals(person),
+            target=Equals(repo.target),
+            name=Equals("%s-1" % repo.name),
+            owner_default=Is(True),
+            target_default=Is(False)))
+        self.assertEqual(
+            [((forked_repo.getInternalPath(),),
+              {"clone_from": None, "async_create": True})],
+            self.hosting_fixture.create.calls)
 
 
 class TestGitRepositoryDetectMerges(TestCaseWithFactory):
