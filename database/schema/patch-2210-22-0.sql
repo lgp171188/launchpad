ff72740be1d391a35e3cf2695a1d5304e32a8a6a
Thiago F Pappacena <thiago.frazao.pappacena@canonical.com>
2020-11-09 16:58:30 -0300

    Updating bugtask_flatten function to update ociproject/ociprojectseries

--- database/schema/patch-2210-22-0.sql
+++ database/schema/patch-2210-22-0.sql
@@ -42,6 +42,118 @@ ALTER TABLE BugSummaryJournal
 
 -- Functions
 
+DROP FUNCTION bugtask_flatten;
+CREATE OR REPLACE FUNCTION bugtask_flatten(task_id integer, check_only boolean)
+    RETURNS boolean
+    LANGUAGE plpgsql SECURITY DEFINER SET search_path = public
+    AS $$
+DECLARE
+    bug_row Bug%ROWTYPE;
+    task_row BugTask%ROWTYPE;
+    old_flat_row BugTaskFlat%ROWTYPE;
+    new_flat_row BugTaskFlat%ROWTYPE;
+    _product_active boolean;
+    _access_policies integer[];
+    _access_grants integer[];
+BEGIN
+    -- This is the master function to update BugTaskFlat, but there are
+    -- maintenance triggers and jobs on the involved tables that update
+    -- it directly. Any changes here probably require a corresponding
+    -- change in other trigger functions.
+
+    SELECT * INTO task_row FROM BugTask WHERE id = task_id;
+    SELECT * INTO old_flat_row FROM BugTaskFlat WHERE bugtask = task_id;
+
+    -- If the task doesn't exist, ensure that there's no flat row.
+    IF task_row.id IS NULL THEN
+        IF old_flat_row.bugtask IS NOT NULL THEN
+            IF NOT check_only THEN
+                DELETE FROM BugTaskFlat WHERE bugtask = task_id;
+            END IF;
+            RETURN FALSE;
+        ELSE
+            RETURN TRUE;
+        END IF;
+    END IF;
+
+    SELECT * FROM bug INTO bug_row WHERE id = task_row.bug;
+
+    -- If it's a product(series) task, we must consult the active flag.
+    IF task_row.product IS NOT NULL THEN
+        SELECT product.active INTO _product_active
+            FROM product WHERE product.id = task_row.product LIMIT 1;
+    ELSIF task_row.productseries IS NOT NULL THEN
+        SELECT product.active INTO _product_active
+            FROM
+                product
+                JOIN productseries ON productseries.product = product.id
+            WHERE productseries.id = task_row.productseries LIMIT 1;
+    END IF;
+
+    SELECT policies, grants
+        INTO _access_policies, _access_grants
+        FROM bug_build_access_cache(bug_row.id, bug_row.information_type)
+            AS (policies integer[], grants integer[]);
+
+    -- Compile the new flat row.
+    SELECT task_row.id, bug_row.id, task_row.datecreated,
+           bug_row.duplicateof, bug_row.owner, bug_row.fti,
+           bug_row.information_type, bug_row.date_last_updated,
+           bug_row.heat, task_row.product, task_row.productseries,
+           task_row.distribution, task_row.distroseries,
+           task_row.sourcepackagename, task_row.status,
+           task_row.importance, task_row.assignee,
+           task_row.milestone, task_row.owner,
+           COALESCE(_product_active, TRUE),
+           _access_policies,
+           _access_grants,
+           bug_row.latest_patch_uploaded, task_row.date_closed,
+           task_row.ociproject, task_row.ociprojectseries
+           INTO new_flat_row;
+
+    -- Calculate the necessary updates.
+    IF old_flat_row.bugtask IS NULL THEN
+        IF NOT check_only THEN
+            INSERT INTO BugTaskFlat VALUES (new_flat_row.*);
+        END IF;
+        RETURN FALSE;
+    ELSIF new_flat_row != old_flat_row THEN
+        IF NOT check_only THEN
+            UPDATE BugTaskFlat SET
+                bug = new_flat_row.bug,
+                datecreated = new_flat_row.datecreated,
+                duplicateof = new_flat_row.duplicateof,
+                bug_owner = new_flat_row.bug_owner,
+                fti = new_flat_row.fti,
+                information_type = new_flat_row.information_type,
+                date_last_updated = new_flat_row.date_last_updated,
+                heat = new_flat_row.heat,
+                product = new_flat_row.product,
+                productseries = new_flat_row.productseries,
+                distribution = new_flat_row.distribution,
+                distroseries = new_flat_row.distroseries,
+                sourcepackagename = new_flat_row.sourcepackagename,
+                status = new_flat_row.status,
+                importance = new_flat_row.importance,
+                assignee = new_flat_row.assignee,
+                milestone = new_flat_row.milestone,
+                owner = new_flat_row.owner,
+                active = new_flat_row.active,
+                access_policies = new_flat_row.access_policies,
+                access_grants = new_flat_row.access_grants,
+                date_closed = new_flat_row.date_closed,
+                latest_patch_uploaded = new_flat_row.latest_patch_uploaded,
+                ociproject = new_flat_row.ociproject,
+                ociprojectseries = new_flat_row.ociprojectseries
+                WHERE bugtask = new_flat_row.bugtask;
+        END IF;
+        RETURN FALSE;
+    ELSE
+        RETURN TRUE;
+    END IF;
+END;
+$$;
+
 CREATE OR REPLACE FUNCTION bugtask_maintain_bugtaskflat_trig()
     RETURNS trigger
     LANGUAGE plpgsql SECURITY DEFINER
