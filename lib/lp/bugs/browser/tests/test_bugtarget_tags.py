fedb421490eb852f78bf1736d950af8b02de0ee5
Thiago F Pappacena <thiago.frazao.pappacena@canonical.com>
2020-11-13 15:55:03 -0300

    Bug tag test scenarios

--- lib/lp/bugs/browser/tests/test_bugtarget_tags.py
+++ lib/lp/bugs/browser/tests/test_bugtarget_tags.py
@@ -1,34 +1,67 @@
-# Copyright 2010-2011 Canonical Ltd.  This software is licensed under the
+# Copyright 2010-2020 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 __metaclass__ = type
+
+from testscenarios.testcase import WithScenarios
 
 from lp.testing import TestCaseWithFactory
 from lp.testing.layers import DatabaseFunctionalLayer
 from lp.testing.views import create_view
 
 
-class TestBugTargetTags(TestCaseWithFactory):
+class TestBugTargetTags(WithScenarios, TestCaseWithFactory):
 
     layer = DatabaseFunctionalLayer
 
+    scenarios = [
+        ("product + group", {"factory_name": 'makeProductAndGroup'}),
+        ("product", {"factory_name": 'makeProduct'}),
+        ("distribution", {"factory_name": 'makeDistribution'}),
+        ("ociproject of product", {
+            "factory_name": 'makeOCIProjectFromProduct'}),
+        ("ociproject of distro", {"factory_name": 'makeOCIProjectFromDistro'}),
+        ]
+
+    def makeProductAndGroup(self):
+        project_group = self.factory.makeProject()
+        product = self.factory.makeProduct(projectgroup=project_group)
+        return project_group, product
+
+    def makeProduct(self):
+        prod = self.factory.makeProduct()
+        return prod, prod
+
+    def makeDistribution(self):
+        distro = self.factory.makeDistribution()
+        return distro, distro
+
+    def makeOCIProjectFromProduct(self):
+        target = self.factory.makeProduct()
+        ociproject = self.factory.makeOCIProject(pillar=target)
+        return ociproject, ociproject
+
+    def makeOCIProjectFromDistro(self):
+        target = self.factory.makeDistribution()
+        ociproject = self.factory.makeOCIProject(pillar=target)
+        return ociproject, ociproject
+
     def setUp(self):
         super(TestBugTargetTags, self).setUp()
-        self.project_group = self.factory.makeProject()
-        self.target_product = self.factory.makeProduct(
-            projectgroup=self.project_group)
+        builder = getattr(self, self.factory_name)
+        self.view_context, self.bug_target = builder()
 
     def test_no_tags(self):
-        self.factory.makeBug(target=self.target_product)
+        self.factory.makeBug(target=self.bug_target)
         view = create_view(
-            self.project_group,
+            self.view_context,
             name="+bugtarget-portlet-tags-content")
         self.assertEqual([], [tag['tag'] for tag in view.tags_cloud_data])
 
     def test_tags(self):
-        self.factory.makeBug(target=self.target_product, tags=[u'foo'])
+        self.factory.makeBug(target=self.bug_target, tags=[u'foo'])
         view = create_view(
-            self.project_group,
+            self.view_context,
             name="+bugtarget-portlet-tags-content")
         self.assertEqual(
             [u'foo'],
@@ -36,15 +69,15 @@ class TestBugTargetTags(TestCaseWithFactory):
 
     def test_tags_order(self):
         """Test that the tags are ordered by most used first"""
-        self.factory.makeBug(target=self.target_product, tags=[u'tag-last'])
+        self.factory.makeBug(target=self.bug_target, tags=[u'tag-last'])
         for counter in range(0, 2):
             self.factory.makeBug(
-                target=self.target_product, tags=[u'tag-middle'])
+                target=self.bug_target, tags=[u'tag-middle'])
         for counter in range(0, 3):
             self.factory.makeBug(
-                target=self.target_product, tags=[u'tag-first'])
+                target=self.bug_target, tags=[u'tag-first'])
         view = create_view(
-            self.project_group,
+            self.view_context,
             name="+bugtarget-portlet-tags-content")
         self.assertEqual(
             [u'tag-first', u'tag-middle', u'tag-last'],
