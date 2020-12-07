4b893d1a62645aa69ea558080931fdf74a145f02
Colin Watson <cjwatson@canonical.com>
2020-12-07 09:43:13 +0000

    Convert LanguagePack to Storm

--- lib/lp/translations/model/languagepack.py
+++ lib/lp/translations/model/languagepack.py
@@ -1,4 +1,4 @@
-# Copyright 2009 Canonical Ltd.  This software is licensed under the
+# Copyright 2009-2020 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Language pack store."""
@@ -10,16 +10,17 @@ __all__ = [
     'LanguagePackSet',
     ]
 
-from sqlobject import ForeignKey
+import pytz
+from storm.locals import (
+    DateTime,
+    Int,
+    Reference,
+    )
 from zope.interface import implementer
 
 from lp.services.database.constants import UTC_NOW
-from lp.services.database.datetimecol import UtcDateTimeCol
-from lp.services.database.enumcol import EnumCol
-from lp.services.database.sqlbase import (
-    SQLBase,
-    sqlvalues,
-    )
+from lp.services.database.enumcol import DBEnum
+from lp.services.database.stormbase import StormBase
 from lp.translations.enums import LanguagePackType
 from lp.translations.interfaces.languagepack import (
     ILanguagePack,
@@ -28,24 +29,34 @@ from lp.translations.interfaces.languagepack import (
 
 
 @implementer(ILanguagePack)
-class LanguagePack(SQLBase):
+class LanguagePack(StormBase):
 
-    _table = 'LanguagePack'
+    __storm_table__ = 'LanguagePack'
 
-    file = ForeignKey(
-        foreignKey='LibraryFileAlias', dbName='file', notNull=True)
+    id = Int(primary=True)
 
-    date_exported = UtcDateTimeCol(notNull=True, default=UTC_NOW)
+    file_id = Int(name='file', allow_none=False)
+    file = Reference(file_id, 'LibraryFileAlias.id')
 
-    distroseries = ForeignKey(
-        foreignKey='DistroSeries', dbName='distroseries', notNull=True)
+    date_exported = DateTime(
+        tzinfo=pytz.UTC, allow_none=False, default=UTC_NOW)
 
-    type = EnumCol(
-        enum=LanguagePackType, notNull=True, default=LanguagePackType.FULL)
+    distroseries_id = Int(name='distroseries', allow_none=False)
+    distroseries = Reference(distroseries_id, 'DistroSeries.id')
 
-    updates = ForeignKey(
-        foreignKey='LanguagePack', dbName='updates',
-        notNull=False, default=None)
+    type = DBEnum(
+        enum=LanguagePackType, allow_none=False, default=LanguagePackType.FULL)
+
+    updates_id = Int(name='updates', allow_none=True, default=None)
+    updates = Reference(updates_id, 'LanguagePack.id')
+
+    def __init__(self, file, date_exported, distroseries, type, updates=None):
+        super(LanguagePack, self).__init__()
+        self.file = file
+        self.date_exported = date_exported
+        self.distroseries = distroseries
+        self.type = type
+        self.updates = updates
 
 
 @implementer(ILanguagePackSet)
@@ -57,11 +68,10 @@ class LanguagePackSet:
             'Unknown language pack type: %s' % type.name)
 
         if (type == LanguagePackType.DELTA and
-            distroseries.language_pack_base is None):
+                distroseries.language_pack_base is None):
             raise AssertionError(
-                "There is no base language pack available for %s's %s to get"
-                " deltas from." % sqlvalues(
-                    distroseries.distribution.name, distroseries.name))
+                "There is no base language pack available for %s to get"
+                " deltas from." % distroseries)
 
         updates = None
         if type == LanguagePackType.DELTA:
