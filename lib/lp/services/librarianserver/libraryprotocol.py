lgp171188/launchpad

commit 2d3fa6f225bea7b4c2393810454cca10439027b7

Convert lp.services.librarianserver to preferred __future__ imports

lib/lp/services/librarianserver/libraryprotocol.py
@@ -1,6 +1,8 @@
 # Copyright 2009-2018 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
+from __future__ import absolute_import, print_function, unicode_literals
+
 __metaclass__ = type
 
 from datetime import datetime
@@ -67,6 +69,10 @@ class FileUploadProtocol(basic.LineReceiver):
 
     def lineReceived(self, line):
         try:
+            try:
+                line = line.decode('UTF-8')
+            except UnicodeDecodeError:
+                raise ProtocolViolation('Non-data lines must be in UTF-8')
             getattr(self, 'line_' + self.state, self.badLine)(line)
         except ProtocolViolation as e:
             self.sendError(e.msg)
@@ -156,11 +162,6 @@ class FileUploadProtocol(basic.LineReceiver):
     def command_STORE(self, args):
         try:
             size, name = args.split(None, 1)
-            try:
-                name = name.decode('utf-8')
-            except:
-                raise ProtocolViolation(
-                    "STORE command expects the filename to be in UTF-8")
             size = int(size)
         except ValueError:
             raise ProtocolViolation(

lib/lp/services/librarianserver/apachelogparser.py
@@ -1,6 +1,8 @@
 # Copyright 2009 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
+from __future__ import absolute_import, print_function, unicode_literals
+
 import re
 
 
@@ -8,17 +10,17 @@ DBUSER = 'librarianlogparser'
 
 
 # Regexp used to match paths to LibraryFileAliases.
-lfa_path_re = re.compile('^/[0-9]+/')
-multi_slashes_re = re.compile('/+')
+lfa_path_re = re.compile(br'^/[0-9]+/')
+multi_slashes_re = re.compile(br'/+')
 
 
 def get_library_file_id(path):
-    path = multi_slashes_re.sub('/', path)
+    path = multi_slashes_re.sub(b'/', path)
     if not lfa_path_re.match(path):
         # We only count downloads of LibraryFileAliases, and this is
         # not one of them.
         return None
 
-    file_id = path.split('/')[1]
+    file_id = path.split(b'/')[1]
     assert file_id.isdigit(), ('File ID is not a digit: %s' % path)
-    return file_id
+    return file_id.decode('UTF-8')

lib/lp/services/librarianserver/db.py
@@ -3,6 +3,8 @@
 
 """Database access layer for the Librarian."""
 
+from __future__ import absolute_import, print_function, unicode_literals
+
 __metaclass__ = type
 __all__ = [
     'Library',

lib/lp/services/librarianserver/librariangc.py
@@ -3,6 +3,8 @@
 
 """Librarian garbage collection routines"""
 
+from __future__ import absolute_import, print_function, unicode_literals
+
 __metaclass__ = type
 
 from datetime import (

lib/lp/services/librarianserver/storage.py
@@ -1,6 +1,8 @@
 # Copyright 2009-2019 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
+from __future__ import absolute_import, print_function, unicode_literals
+
 __metaclass__ = type
 
 import errno

lib/lp/services/librarianserver/swift.py
@@ -3,6 +3,8 @@
 
 """Move files from Librarian disk storage into Swift."""
 
+from __future__ import absolute_import, print_function, unicode_literals
+
 __metaclass__ = type
 __all__ = [
     'SWIFT_CONTAINER_PREFIX',

lib/lp/services/librarianserver/testing/fake.py
@@ -10,6 +10,8 @@ provides a simple and fast alternative to the full Librarian in unit
 tests.
 """
 
+from __future__ import absolute_import, print_function, unicode_literals
+
 __metaclass__ = type
 __all__ = [
     'FakeLibrarian',

lib/lp/services/librarianserver/testing/server.py
@@ -3,6 +3,8 @@
 
 """Fixture for the librarians."""
 
+from __future__ import absolute_import, print_function, unicode_literals
+
 __metaclass__ = type
 __all__ = [
     'fillLibrarianFile',

commit 3e9c63cd0fa56cd5e1d90dea656e17a39d29287a

Remove excessive decoding from FileUploadProtocol
Broken by 2d3fa6f225bea7b4c2393810454cca10439027b7, because lineReceived
now decodes the line first so we shouldn't decode it again later.  We
get away with this on Python 2 because (confusingly) it's usually
possible to decode a unicode object there as long as it only contains
ASCII, but Python 3 is stricter.

lib/lp/services/librarianserver/libraryprotocol.py
@@ -111,7 +111,6 @@ class FileUploadProtocol(basic.LineReceiver):
     def line_command(self, line):
         try:
             command, args = line.split(None, 1)
-            command = command.decode('UTF-8')
         except ValueError:
             raise ProtocolViolation('Bad command: ' + line)
 
@@ -120,7 +119,7 @@ class FileUploadProtocol(basic.LineReceiver):
 
     def line_header(self, line):
         # Blank line signals the end of the headers
-        if line == b'':
+        if line == '':
             # If File-Content-ID was specified, File-Alias-ID must be too, and
             # vice-versa.
             contentID = self.newFile.contentID
@@ -147,7 +146,7 @@ class FileUploadProtocol(basic.LineReceiver):
 
         # Simple RFC 822-ish header parsing
         try:
-            name, value = line.decode('UTF-8').split(':', 2)
+            name, value = line.split(':', 2)
         except ValueError:
             raise ProtocolViolation('Invalid header: ' + line)
 