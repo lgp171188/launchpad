30af836e4a6b9cf5211643500a60e9b1c8dd6321
Colin Watson <cjwatson@canonical.com>
2021-01-27 11:33:48 +0000

    Ensure that private teams are visible on +sharing
    Product:+sharing and Distribution:+sharing are only visible to pillar
    drivers, who should be able to see who has access to their pillar.
    However, it's possible to end up with a pillar having a grant to a
    private team that not all the drivers can see.  In some ways it might be
    best to grant LimitedView in this case, but doing that in the security
    adapter would be risky for performance as that adapter is already very
    complex and often slow.  Instead, precache LimitedView permissions for
    all grantees just for PillarSharingView.
    
    LP: #1913334

--- lib/lp/registry/browser/pillar.py
+++ lib/lp/registry/browser/pillar.py
@@ -1,4 +1,4 @@
-# Copyright 2009-2015 Canonical Ltd.  This software is licensed under the
+# Copyright 2009-2021 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Common views for objects that implement `IPillar`."""
@@ -57,7 +57,10 @@ from lp.registry.interfaces.projectgroup import IProjectGroup
 from lp.registry.model.pillar import PillarPerson
 from lp.services.config import config
 from lp.services.propertycache import cachedproperty
-from lp.services.webapp.authorization import check_permission
+from lp.services.webapp.authorization import (
+    check_permission,
+    precache_permission_for_objects,
+    )
 from lp.services.webapp.batching import (
     BatchNavigator,
     get_batch_properties_for_json_cache,
@@ -376,6 +379,14 @@ class PillarSharingView(LaunchpadView):
         cache.objects['has_edit_permission'] = check_permission(
             "launchpad.Edit", self.context)
         batch_navigator = self.grantees()
+        # Precache LimitedView for all the grantees, partly for performance
+        # but mainly because it's possible that the user won't strictly have
+        # LimitedView on all of them and they should nevertheless be able to
+        # see who has access to pillars they drive.  Fixing this in
+        # PublicOrPrivateTeamsExistence would very likely be too expensive.
+        precache_permission_for_objects(
+            None, 'launchpad.LimitedView',
+            [grantee for grantee, _, _ in batch_navigator.batch])
         cache.objects['grantee_data'] = (
             self._getSharingService().jsonGranteeData(batch_navigator.batch))
         cache.objects.update(
