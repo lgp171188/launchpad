c40284de5c73c9fb3bc8953ea021d780d5d2c7ae
Colin Watson <cjwatson@canonical.com>
2021-02-22 23:47:45 +0000

    Fix handling of removed objects in StormBase
    Objects that have been removed don't have `primary_vars`, and `Store.of`
    returns None for them.  This caused `StormBase.__eq__` (and, less
    importantly, `StormBase.__hash__`) to crash if given such an object.  In
    most cases we don't keep removed objects around, but there is at least
    one exception: the cache of the current `BuildQueue` associated with the
    builder managed by a `SlaveScanner`, which is checked for equality with
    the `BuildQueue` found from the builder's vitals.
    
    To avoid this problem, use `IStore(obj.__class__).flush()` instead, and
    in the `__eq__` case explicitly return False if this still doesn't cause
    `primary_vars` to appear.  (`__hash__` will still crash if used on a
    removed object, but that seems to be OK.)  This is a better match for
    the details of the behaviour of `SQLBase`.
    
    Fixes a regression introduced by
    6f2a36c1e74d99f511b033c19273c4cb2b67c6c4.
    
    LP: #1916522

--- lib/lp/services/database/stormbase.py
+++ lib/lp/services/database/stormbase.py
@@ -7,12 +7,10 @@ __all__ = [
     ]
 
 from storm.info import get_obj_info
-from storm.locals import (
-    Store,
-    Storm,
-    )
+from storm.locals import Storm
 from zope.security.proxy import removeSecurityProxy
 
+from lp.services.database.interfaces import IStore
 from lp.services.propertycache import clear_property_cache
 
 
@@ -25,12 +23,16 @@ class StormBase(Storm):
     def __eq__(self, other):
         """Equality operator.
 
-        Objects compare equal if they have the same class and id, and the id
-        is not None.
+        Objects compare equal if they have the same class and primary key,
+        and the primary key is not None.
 
         This rule allows objects retrieved from different stores to compare
-        equal.  Newly-created objects may not yet have an id; in such cases
-        we flush the store so that we can find out their id.
+        equal.  Newly-created objects may not yet have an primary key; in
+        such cases we flush the store so that we can find out their primary
+        key.  Objects that have been removed from the store will have no
+        primary key even after flushing the store, and compare unequal to
+        anything (although keeping objects around after they have been
+        removed is not normally a good idea).
         """
         other = removeSecurityProxy(other)
         if self.__class__ != other.__class__:
@@ -38,9 +40,13 @@ class StormBase(Storm):
         self_obj_info = get_obj_info(self)
         other_obj_info = get_obj_info(other)
         if "primary_vars" not in self_obj_info:
-            Store.of(self).flush()
+            IStore(self.__class__).flush()
+            if "primary_vars" not in self_obj_info:
+                return False
         if "primary_vars" not in other_obj_info:
-            Store.of(other).flush()
+            IStore(other.__class__).flush()
+            if "primary_vars" not in other_obj_info:
+                return False
         self_primary = [var.get() for var in self_obj_info["primary_vars"]]
         other_primary = [var.get() for var in other_obj_info["primary_vars"]]
         return self_primary == other_primary
@@ -60,7 +66,7 @@ class StormBase(Storm):
         """
         obj_info = get_obj_info(self)
         if "primary_vars" not in obj_info:
-            Store.of(self).flush()
+            IStore(self.__class__).flush()
         primary = [var.get() for var in obj_info["primary_vars"]]
         return hash((self.__class__,) + tuple(primary))
 
