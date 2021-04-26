c4d60c0fbc0f4b14e024c8fa680ee728cd7ae5bc
Colin Watson <cjwatson@canonical.com>
2021-04-26 08:17:57 +0100

    Add script to sync branches from production to staging
    We currently have a "codehosting_branch_mirror.py" script on (qa)staging
    that rsyncs a few branches from production for testing.  This is a
    Python script that uses Launchpad's virtualenv, imports our modules, and
    talks to our database.  As a result, it can easily be broken by changes
    in Launchpad: in particular, it was broken by moving to Python 3.
    
    Bring a version of this script into our tree so that we can make sure it
    stays working.  I modified it considerably, mainly to allow selecting
    branches on the command line, and added tests.

---
+++ scripts/sync-branches.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python2 -S
+# Copyright 2021 Canonical Ltd.  All rights reserved.
+
+"""Sync branches from production to a staging environment."""
+
+import _pythonpath
+
+from lp.codehosting.scripts.sync_branches import SyncBranchesScript
+
+
+if __name__ == "__main__":
+    script = SyncBranchesScript("sync-branches", dbuser="ro")
+    script.lock_and_run()
