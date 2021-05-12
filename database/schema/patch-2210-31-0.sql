3c88b3beceee49ed55c71feab6e097a3e60bcebd
Thiago F Pappacena <thiago.frazao.pappacena@canonical.com>
2021-05-12 14:05:46 -0300

    Adding MessageRevisionChunk

--- database/schema/patch-2210-31-0.sql
+++ database/schema/patch-2210-31-0.sql
@@ -5,17 +5,24 @@ SET client_min_messages=ERROR;
 
 ALTER TABLE Message
     ADD COLUMN date_deleted timestamp without time zone,
-    ADD COLUMN date_last_edit timestamp without time zone;
+    ADD COLUMN date_last_edited timestamp without time zone;
 
 CREATE TABLE MessageRevision (
     id serial PRIMARY KEY,
     message integer NOT NULL REFERENCES Message,
-    content text,
     date_created timestamp without time zone,
     date_deleted timestamp without time zone
 );
 
 CREATE UNIQUE INDEX messagerevision__message__date_created__key
     ON MessageRevision(message, date_created);
+
+CREATE TABLE MessageRevisionChunk (
+    id serial PRIMARY KEY,
+    messagerevision integer NOT NULL REFERENCES MessageRevisionChunk,
+    sequence integer NOT NULL,
+    content text NOT NULL
+);
+
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2210, 31, 0);
