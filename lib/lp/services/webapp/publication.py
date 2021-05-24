554ff40afb33a9023fc35a207135269251f67075
Colin Watson <cjwatson@canonical.com>
2021-05-24 11:11:49 +0100

    Discard previous interaction in endRequest
    Zope's `BrowserPublication.endRequest` leaves a reference to the
    previous interaction lying around in thread-local storage just in case
    somebody might want to call `restoreInteraction` later.  However, this
    significantly complicates cyclic garbage collection and memory leak
    analysis, because it means that objects referenced by a request can
    never be garbage-collected immediately after the request finishes, but
    at best only somewhat later.
    
    There are cases in Launchpad where we do use `restoreInteraction`, but
    none of them are after the end of publishing a request, so simplify
    matters by discarding the previous interaction.

--- lib/lp/services/webapp/publication.py
+++ lib/lp/services/webapp/publication.py
@@ -52,7 +52,10 @@ from zope.publisher.interfaces.browser import (
     IDefaultSkin,
     )
 from zope.publisher.publish import mapply
-from zope.security.management import newInteraction
+from zope.security.management import (
+    endInteraction,
+    newInteraction,
+    )
 from zope.security.proxy import (
     isinstance as zope_isinstance,
     removeSecurityProxy,
@@ -785,6 +788,15 @@ class LaunchpadBrowserPublication(
     def endRequest(self, request, object):
         superclass = zope.app.publication.browser.BrowserPublication
         superclass.endRequest(self, request, object)
+        # BrowserPublication.endRequest calls endInteraction as well, but
+        # calling it once leaves a reference to the previous interaction
+        # around in
+        # zope.security.management.thread_local.previous_interaction just in
+        # case somebody might want to call restoreInteraction later,
+        # significantly complicating memory leak analysis.  We won't need to
+        # restore the previous interaction in this case, so call
+        # endInteraction again to discard it.
+        endInteraction()
 
         da.clear_request_started()
 
