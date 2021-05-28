f6578dab0433d9c5c38b69911bf61186bccb786a
Colin Watson <cjwatson@canonical.com>
2021-05-28 15:30:33 +0100

    Add basic model for charm recipe jobs
    This job is a placeholder and doesn't actually do anything yet: we'll
    need a model for charm recipe builds first.  I'm adding the job first
    because I want to take the more modern approach of all charm recipe
    builds having an associated build request.
    
    We'll need to deploy a "charm-build-job" DB user to production before
    landing this.

---
+++ lib/lp/charms/interfaces/charmrecipejob.py
@@ -0,0 +1,122 @@
+# Copyright 2021 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Charm recipe job interfaces."""
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+__metaclass__ = type
+__all__ = [
+    "ICharmRecipeJob",
+    "ICharmRecipeRequestBuildsJob",
+    "ICharmRecipeRequestBuildsJobSource",
+    ]
+
+from lazr.restful.fields import Reference
+from zope.interface import (
+    Attribute,
+    Interface,
+    )
+from zope.schema import (
+    Datetime,
+    Dict,
+    Set,
+    TextLine,
+    )
+
+from lp import _
+from lp.charms.interfaces.charmrecipe import (
+    ICharmRecipe,
+    ICharmRecipeBuildRequest,
+    )
+from lp.registry.interfaces.person import IPerson
+from lp.services.job.interfaces.job import (
+    IJob,
+    IJobSource,
+    IRunnableJob,
+    )
+
+
+class ICharmRecipeJob(Interface):
+    """A job related to a charm recipe."""
+
+    job = Reference(
+        title=_("The common Job attributes."), schema=IJob,
+        required=True, readonly=True)
+
+    recipe = Reference(
+        title=_("The charm recipe to use for this job."),
+        schema=ICharmRecipe, required=True, readonly=True)
+
+    metadata = Attribute(_("A dict of data about the job."))
+
+
+class ICharmRecipeRequestBuildsJob(IRunnableJob):
+    """A Job that processes a request for builds of a charm recipe."""
+
+    requester = Reference(
+        title=_("The person requesting the builds."), schema=IPerson,
+        required=True, readonly=True)
+
+    channels = Dict(
+        title=_("Source snap channels to use for these builds."),
+        description=_(
+            "A dictionary mapping snap names to channels to use for these "
+            "builds.  Currently only 'core', 'core18', 'core20', and "
+            "'charmcraft' keys are supported."),
+        key_type=TextLine(), required=False, readonly=True)
+
+    architectures = Set(
+        title=_("If set, limit builds to these architecture tags."),
+        value_type=TextLine(), required=False, readonly=True)
+
+    date_created = Datetime(
+        title=_("Time when this job was created."),
+        required=True, readonly=True)
+
+    date_finished = Datetime(
+        title=_("Time when this job finished."),
+        required=True, readonly=True)
+
+    error_message = TextLine(
+        title=_("Error message resulting from running this job."),
+        required=False, readonly=True)
+
+    build_request = Reference(
+        title=_("The build request corresponding to this job."),
+        schema=ICharmRecipeBuildRequest, required=True, readonly=True)
+
+
+class ICharmRecipeRequestBuildsJobSource(IJobSource):
+
+    def create(recipe, requester, channels=None, architectures=None):
+        """Request builds of a charm recipe.
+
+        :param recipe: The charm recipe to build.
+        :param requester: The person requesting the builds.
+        :param channels: A dictionary mapping snap names to channels to use
+            for these builds.
+        :param architectures: If not None, limit builds to architectures
+            with these architecture tags (in addition to any other
+            applicable constraints).
+        """
+
+    def findByRecipe(recipe, statuses=None, job_ids=None):
+        """Find jobs for a charm recipe.
+
+        :param recipe: A charm recipe to search for.
+        :param statuses: An optional iterable of `JobStatus`es to search for.
+        :param job_ids: An optional iterable of job IDs to search for.
+        :return: A sequence of `CharmRecipeRequestBuildsJob`s with the
+            specified recipe.
+        """
+
+    def getByRecipeAndID(recipe, job_id):
+        """Get a job by charm recipe and job ID.
+
+        :return: The `CharmRecipeRequestBuildsJob` with the specified recipe
+            and ID.
+        :raises: `NotFoundError` if there is no job with the specified
+            recipe and ID, or its `job_type` is not
+            `CharmRecipeJobType.REQUEST_BUILDS`.
+        """
--- lib/lp/charms/tests/test_charmrecipe.py
+++ lib/lp/charms/tests/test_charmrecipe.py
@@ -7,23 +7,37 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 __metaclass__ = type
 
+from testtools.matchers import (
+    Equals,
+    Is,
+    MatchesDict,
+    MatchesSetwise,
+    MatchesStructure,
+    )
 from zope.component import getUtility
 from zope.security.proxy import removeSecurityProxy
 
 from lp.app.enums import InformationType
 from lp.charms.interfaces.charmrecipe import (
     CHARM_RECIPE_ALLOW_CREATE,
+    CharmRecipeBuildRequestStatus,
     CharmRecipeFeatureDisabled,
     CharmRecipePrivateFeatureDisabled,
     ICharmRecipe,
     ICharmRecipeSet,
     NoSourceForCharmRecipe,
     )
+from lp.charms.interfaces.charmrecipejob import (
+    ICharmRecipeRequestBuildsJobSource,
+    )
 from lp.services.database.constants import (
     ONE_DAY_AGO,
     UTC_NOW,
     )
+from lp.services.database.interfaces import IStore
+from lp.services.database.sqlbase import get_transaction_timestamp
 from lp.services.features.testing import FeatureFixture
+from lp.services.job.interfaces.job import JobStatus
 from lp.services.webapp.snapshot import notify_modified
 from lp.testing import (
     admin_logged_in,
@@ -89,6 +103,80 @@ class TestCharmRecipe(TestCaseWithFactory):
             pass
         self.assertSqlAttributeEqualsDate(
             recipe, "date_last_modified", UTC_NOW)
+
+    def test_requestBuilds(self):
+        # requestBuilds schedules a job and returns a corresponding
+        # CharmRecipeBuildRequest.
+        recipe = self.factory.makeCharmRecipe()
+        now = get_transaction_timestamp(IStore(recipe))
+        with person_logged_in(recipe.owner.teamowner):
+            request = recipe.requestBuilds(recipe.owner.teamowner)
+        self.assertThat(request, MatchesStructure(
+            date_requested=Equals(now),
+            date_finished=Is(None),
+            recipe=Equals(recipe),
+            status=Equals(CharmRecipeBuildRequestStatus.PENDING),
+            error_message=Is(None),
+            channels=Is(None),
+            architectures=Is(None)))
+        [job] = getUtility(ICharmRecipeRequestBuildsJobSource).iterReady()
+        self.assertThat(job, MatchesStructure(
+            job_id=Equals(request.id),
+            job=MatchesStructure.byEquality(status=JobStatus.WAITING),
+            recipe=Equals(recipe),
+            requester=Equals(recipe.owner.teamowner),
+            channels=Is(None),
+            architectures=Is(None)))
+
+    def test_requestBuilds_with_channels(self):
+        # If asked to build using particular snap channels, requestBuilds
+        # passes those through to the job.
+        recipe = self.factory.makeCharmRecipe()
+        now = get_transaction_timestamp(IStore(recipe))
+        with person_logged_in(recipe.owner.teamowner):
+            request = recipe.requestBuilds(
+                recipe.owner.teamowner, channels={"charmcraft": "edge"})
+        self.assertThat(request, MatchesStructure(
+            date_requested=Equals(now),
+            date_finished=Is(None),
+            recipe=Equals(recipe),
+            status=Equals(CharmRecipeBuildRequestStatus.PENDING),
+            error_message=Is(None),
+            channels=MatchesDict({"charmcraft": Equals("edge")}),
+            architectures=Is(None)))
+        [job] = getUtility(ICharmRecipeRequestBuildsJobSource).iterReady()
+        self.assertThat(job, MatchesStructure(
+            job_id=Equals(request.id),
+            job=MatchesStructure.byEquality(status=JobStatus.WAITING),
+            recipe=Equals(recipe),
+            requester=Equals(recipe.owner.teamowner),
+            channels=Equals({"charmcraft": "edge"}),
+            architectures=Is(None)))
+
+    def test_requestBuilds_with_architectures(self):
+        # If asked to build for particular architectures, requestBuilds
+        # passes those through to the job.
+        recipe = self.factory.makeCharmRecipe()
+        now = get_transaction_timestamp(IStore(recipe))
+        with person_logged_in(recipe.owner.teamowner):
+            request = recipe.requestBuilds(
+                recipe.owner.teamowner, architectures={"amd64", "i386"})
+        self.assertThat(request, MatchesStructure(
+            date_requested=Equals(now),
+            date_finished=Is(None),
+            recipe=Equals(recipe),
+            status=Equals(CharmRecipeBuildRequestStatus.PENDING),
+            error_message=Is(None),
+            channels=Is(None),
+            architectures=MatchesSetwise(Equals("amd64"), Equals("i386"))))
+        [job] = getUtility(ICharmRecipeRequestBuildsJobSource).iterReady()
+        self.assertThat(job, MatchesStructure(
+            job_id=Equals(request.id),
+            job=MatchesStructure.byEquality(status=JobStatus.WAITING),
+            recipe=Equals(recipe),
+            requester=Equals(recipe.owner.teamowner),
+            channels=Is(None),
+            architectures=MatchesSetwise(Equals("amd64"), Equals("i386"))))
 
     def test_delete_without_builds(self):
         # A charm recipe with no builds can be deleted.
