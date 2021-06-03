a24958d283219938951781d1a9525c793404469e
Colin Watson <cjwatson@canonical.com>
2021-06-03 20:11:51 +0100

    Add a charmcraft.yaml parser
    Parse the data from charmcraft.yaml and use it to determine which
    distroarchseries to dispatch builds to.  This is somewhat similar to our
    snapcraft.yaml parser, but charmcraft.yaml specifies series (although it
    calls them "channels") as well as architectures, so we need to handle
    those too.

---
+++ lib/lp/charms/adapters/tests/test_buildarch.py
@@ -0,0 +1,312 @@
+# Copyright 2021 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+__metaclass__ = type
+
+from functools import partial
+
+from testscenarios import (
+    load_tests_apply_scenarios,
+    WithScenarios,
+    )
+from testtools.matchers import (
+    Equals,
+    MatchesException,
+    MatchesListwise,
+    MatchesStructure,
+    Raises,
+    )
+from zope.component import getUtility
+
+from lp.app.interfaces.launchpad import ILaunchpadCelebrities
+from lp.buildmaster.interfaces.processor import (
+    IProcessorSet,
+    ProcessorNotFound,
+    )
+from lp.charms.adapters.buildarch import (
+    CharmBase,
+    CharmBaseConfiguration,
+    determine_instances_to_build,
+    DuplicateRunOnError,
+    )
+from lp.testing import (
+    TestCase,
+    TestCaseWithFactory,
+    )
+from lp.testing.layers import LaunchpadZopelessLayer
+
+
+class TestCharmBaseConfiguration(WithScenarios, TestCase):
+
+    scenarios = [
+        ("expanded", {
+            "base": {
+                "build-on": [{
+                    "name": "ubuntu",
+                    "channel": "18.04",
+                    "architectures": ["amd64"],
+                    }],
+                "run-on": [
+                    {
+                        "name": "ubuntu",
+                        "channel": "20.04",
+                        "architectures": ["amd64", "arm64"],
+                        },
+                    {
+                        "name": "ubuntu",
+                        "channel": "18.04",
+                        "architectures": ["amd64"],
+                        },
+                    ],
+                },
+            "expected_build_on": [
+                CharmBase(
+                    name="ubuntu", channel="18.04", architectures=["amd64"]),
+                ],
+            "expected_run_on": [
+                CharmBase(
+                    name="ubuntu", channel="20.04",
+                    architectures=["amd64", "arm64"]),
+                CharmBase(
+                    name="ubuntu", channel="18.04", architectures=["amd64"]),
+                ],
+            }),
+        ("short form", {
+            "base": {
+                "name": "ubuntu",
+                "channel": "20.04",
+                },
+            "expected_build_on": [CharmBase(name="ubuntu", channel="20.04")],
+            "expected_run_on": [CharmBase(name="ubuntu", channel="20.04")],
+            }),
+        ("no run-on", {
+            "base": {
+                "build-on": [{
+                    "name": "ubuntu",
+                    "channel": "20.04",
+                    "architectures": ["amd64"],
+                    }],
+                },
+            "expected_build_on": [
+                CharmBase(
+                    name="ubuntu", channel="20.04", architectures=["amd64"]),
+                ],
+            "expected_run_on": [
+                CharmBase(
+                    name="ubuntu", channel="20.04", architectures=["amd64"]),
+                ],
+            }),
+        ]
+
+    def test_base(self):
+        config = CharmBaseConfiguration.from_dict(self.base)
+        self.assertEqual(self.expected_build_on, config.build_on)
+        self.assertEqual(self.expected_run_on, config.run_on)
+
+
+class TestDetermineInstancesToBuild(WithScenarios, TestCaseWithFactory):
+
+    layer = LaunchpadZopelessLayer
+
+    # Scenarios taken from the charmcraft build providers specification:
+    # https://docs.google.com/document/d/1Tix0V2J21hfXj-dukgArbTEN8rg31b7za4VM_KylDkc
+    scenarios = [
+        ("single entry, single arch", {
+            "bases": [{
+                "build-on": [{
+                    "name": "ubuntu",
+                    "channel": "18.04",
+                    "architectures": ["amd64"],
+                    }],
+                "run-on": [{
+                    "name": "ubuntu",
+                    "channel": "18.04",
+                    "architectures": ["amd64"],
+                    }],
+                }],
+            "expected": [("18.04", "amd64")],
+            }),
+        ("multiple entries, single arch", {
+            "bases": [
+                {
+                    "build-on": [{
+                        "name": "ubuntu",
+                        "channel": "18.04",
+                        "architectures": ["amd64"],
+                        }],
+                    "run-on": [{
+                        "name": "ubuntu",
+                        "channel": "18.04",
+                        "architectures": ["amd64"],
+                        }],
+                    },
+                {
+                    "build-on": [{
+                        "name": "ubuntu",
+                        "channel": "20.04",
+                        "architectures": ["amd64"],
+                        }],
+                    "run-on": [{
+                        "name": "ubuntu",
+                        "channel": "20.04",
+                        "architectures": ["amd64"],
+                        }],
+                    },
+                {
+                    "build-on": [{
+                        "name": "ubuntu",
+                        "channel": "20.04",
+                        "architectures": ["riscv64"],
+                        }],
+                    "run-on": [{
+                        "name": "ubuntu",
+                        "channel": "20.04",
+                        "architectures": ["riscv64"],
+                        }],
+                    },
+                ],
+            "expected": [
+                ("18.04", "amd64"), ("20.04", "amd64"), ("20.04", "riscv64")],
+            }),
+        ("single entry, multiple arches", {
+            "bases": [{
+                "build-on": [{
+                    "name": "ubuntu",
+                    "channel": "20.04",
+                    "architectures": ["amd64"],
+                    }],
+                "run-on": [{
+                    "name": "ubuntu",
+                    "channel": "20.04",
+                    "architectures": ["amd64", "riscv64"],
+                    }],
+                }],
+            "expected": [("20.04", "amd64")],
+            }),
+        ("multiple entries, with cross-arch", {
+            "bases": [
+                {
+                    "build-on": [{
+                        "name": "ubuntu",
+                        "channel": "20.04",
+                        "architectures": ["amd64"],
+                        }],
+                    "run-on": [{
+                        "name": "ubuntu",
+                        "channel": "20.04",
+                        "architectures": ["riscv64"],
+                        }],
+                    },
+                {
+                    "build-on": [{
+                        "name": "ubuntu",
+                        "channel": "20.04",
+                        "architectures": ["amd64"],
+                        }],
+                    "run-on": [{
+                        "name": "ubuntu",
+                        "channel": "20.04",
+                        "architectures": ["amd64"],
+                        }],
+                    },
+                ],
+            "expected": [("20.04", "amd64")],
+            }),
+        ("multiple run-on entries", {
+            "bases": [{
+                "build-on": [{
+                    "name": "ubuntu",
+                    "channel": "20.04",
+                    "architectures": ["amd64"],
+                    }],
+                "run-on": [
+                    {
+                        "name": "ubuntu",
+                        "channel": "18.04",
+                        "architectures": ["amd64"],
+                        },
+                    {
+                        "name": "ubuntu",
+                        "channel": "20.04",
+                        "architectures": ["amd64", "riscv64"],
+                        },
+                    ],
+                }],
+            "expected": [("20.04", "amd64")],
+            }),
+        ("redundant outputs", {
+            "bases": [
+                {
+                    "build-on": [{
+                        "name": "ubuntu",
+                        "channel": "18.04",
+                        "architectures": ["amd64"],
+                        }],
+                    "run-on": [{
+                        "name": "ubuntu",
+                        "channel": "20.04",
+                        "architectures": ["amd64"],
+                        }],
+                    },
+                {
+                    "build-on": [{
+                        "name": "ubuntu",
+                        "channel": "20.04",
+                        "architectures": ["amd64"],
+                        }],
+                    "run-on": [{
+                        "name": "ubuntu",
+                        "channel": "20.04",
+                        "architectures": ["amd64"],
+                        }],
+                    },
+                ],
+            "expected_exception": MatchesException(
+                DuplicateRunOnError,
+                r"ubuntu 20\.04 \[\"amd64\"\] is present in the 'run-on' of "
+                r"multiple items"),
+            }),
+        ("no bases specified", {
+            "bases": None,
+            "expected": [
+                ("20.04", "amd64"), ("20.04", "arm64"), ("20.04", "riscv64")],
+            }),
+        ]
+
+    def test_parser(self):
+        distro_serieses = [
+            self.factory.makeDistroSeries(
+                distribution=getUtility(ILaunchpadCelebrities).ubuntu,
+                version=version)
+            for version in ("20.04", "18.04")]
+        dases = []
+        for arch_tag in ("amd64", "arm64", "riscv64"):
+            try:
+                processor = getUtility(IProcessorSet).getByName(arch_tag)
+            except ProcessorNotFound:
+                processor = self.factory.makeProcessor(
+                    name=arch_tag, supports_virtualized=True)
+            for distro_series in distro_serieses:
+                dases.append(self.factory.makeDistroArchSeries(
+                    distroseries=distro_series, architecturetag=arch_tag,
+                    processor=processor))
+        charmcraft_data = {}
+        if self.bases is not None:
+            charmcraft_data["bases"] = self.bases
+        build_instances_factory = partial(
+            determine_instances_to_build,
+            charmcraft_data, dases, distro_serieses[0])
+        if hasattr(self, "expected_exception"):
+            self.assertThat(
+                build_instances_factory, Raises(self.expected_exception))
+        else:
+            self.assertThat(build_instances_factory(), MatchesListwise([
+                MatchesStructure(
+                    distroseries=MatchesStructure.byEquality(version=version),
+                    architecturetag=Equals(arch_tag))
+                for version, arch_tag in self.expected]))
+
+
+load_tests = load_tests_apply_scenarios
