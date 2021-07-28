601c07fe9ce45ddaeb5cbf3494f49cef8657db6d
Tom Wardill <tom.wardill@canonical.com>
2021-07-28 10:18:41 +0000

    Add affirmed column to signedcodeofconduct

---
+++ database/schema/patch-2210-34-0.sql
@@ -0,0 +1,11 @@
+-- Copyright 2021 Canonical Ltd.  This software is licensed under the
+-- GNU Affero General Public License version 3 (see the file LICENSE).
+
+SET client_min_messages=ERROR;
+
+ALTER TABLE signedcodeofconduct
+    ADD COLUMN affirmed boolean DEFAULT false;
+
+COMMENT ON COLUMN signedcodeofconduct.affirmed IS 'Code of conduct was affirmed via website interaction.';
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2210, 34, 0);
