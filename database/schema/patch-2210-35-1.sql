dc4adfa0b497eedd6d51327a59af5edce1d40897
Ioana Lasc <ioana.lasc@canonical.com>
2021-08-27 14:20:30 +0300

    Indent second line

--- database/schema/patch-2210-35-1.sql
+++ database/schema/patch-2210-35-1.sql
@@ -4,6 +4,6 @@
 SET client_min_messages=ERROR;
 
 CREATE INDEX snapbuild__snap__store_upload_revision__idx
-ON SnapBuild(snap, store_upload_revision);
+    ON SnapBuild(snap, store_upload_revision);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2210, 35, 1);
