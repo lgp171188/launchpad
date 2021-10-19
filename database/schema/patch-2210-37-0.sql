ca2b76dc7702a899c75efcdf37ff2c6195f021cd
Ioana Lasc <ioana.lasc@canonical.com>
2021-10-19 16:31:44 +0300

    Address code review comments.

--- database/schema/patch-2210-37-0.sql
+++ database/schema/patch-2210-37-0.sql
@@ -8,43 +8,45 @@ CREATE TABLE RevisionStatusReport (
     name text NOT NULL,
     git_repository integer REFERENCES gitrepository NOT NULL,
     commit_sha1 character(40) NOT NULL,
-    status INTEGER NOT NULL,
-    external_url text,
+    url text,
     description text,
     result integer,
     date_created timestamp without time zone DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL,
-    date_started timestamp without time zone DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL,
-    date_finished timestamp without time zone
+    date_started timestamp without time zone,
+    date_finished timestamp without time zone,
+    log integer
 );
 
-COMMENT ON TABLE RevisionStatusReport IS 'This table links the submitted reports to the target git references.';
+COMMENT ON TABLE RevisionStatusReport IS 'A status check for a code revision.';
 COMMENT ON COLUMN RevisionStatusReport.name IS 'Name of the report.';
-COMMENT ON COLUMN RevisionStatusReport.status IS 'One of the RevisionStatus enum values: Queued, Started, Completed, FailedToStart.';
-COMMENT ON COLUMN RevisionStatusReport.external_url IS 'External URL to view result of report.';
+COMMENT ON COLUMN RevisionStatusReport.url IS 'External URL to view result of report.';
 COMMENT ON COLUMN RevisionStatusReport.description IS 'Text description of the result.';
 COMMENT ON COLUMN RevisionStatusReport.git_repository IS 'Reference to the GitRepository.';
 COMMENT ON COLUMN RevisionStatusReport.commit_sha1 IS 'The commit sha1 for the report.';
-COMMENT ON COLUMN RevisionStatusReport.result IS 'One of the RevisionStatusResult enum values: Success, Failed, Skipped, Cancelled.';
+COMMENT ON COLUMN RevisionStatusReport.result IS 'The result of the check job for this revision.';
 COMMENT ON COLUMN RevisionStatusReport.date_created IS 'DateTime that report was created.';
 COMMENT ON COLUMN RevisionStatusReport.date_started IS 'DateTime that report was started.';
 COMMENT ON COLUMN RevisionStatusReport.date_finished IS 'DateTime that report was completed.';
 
-CREATE INDEX revision_status_report__git_repository__idx
-    ON RevisionStatusReport (git_repository);
+CREATE INDEX revision_status_report__git_repository__idx_commit_sha1__name
+    ON RevisionStatusReport (git_repository, commit_sha1, name);
 
 CREATE TABLE RevisionStatusArtifact (
     id serial PRIMARY KEY,
     library_file integer REFERENCES libraryfilealias,
-    revision_status integer REFERENCES RevisionStatusReport
+    report integer REFERENCES RevisionStatusReport
 );
 
 COMMENT ON TABLE RevisionStatusArtifact IS 'A thin wrapper around LibraryFileAlias.';
-COMMENT ON COLUMN RevisionStatusArtifact.library_file IS 'Reference to LibraryFileAlias.';
-COMMENT ON COLUMN RevisionStatusArtifact.revision_status IS 'Reference to RevisionStatusReport.';
+COMMENT ON COLUMN RevisionStatusArtifact.library_file IS 'LibraryFileAlias storing the contents of the artifact.';
+COMMENT ON COLUMN RevisionStatusArtifact.report IS 'Stores a link back to the report that the artifact was produced by.';
 
 CREATE INDEX revision_status_artifact__library_file__idx
     ON RevisionStatusArtifact (library_file);
-CREATE INDEX revision_status_artifact__revision_status__idx
-    ON RevisionStatusArtifact (revision_status);
+CREATE INDEX revision_status_artifact__report__idx
+    ON RevisionStatusArtifact (report);
+
+ALTER TABLE RevisionStatusReport
+    ADD CONSTRAINT log_fkey FOREIGN KEY (log) REFERENCES RevisionStatusArtifact (id);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2210, 37, 0);
