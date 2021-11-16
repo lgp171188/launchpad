2fd70b8d4840179b27e0ee973a9c27a4855c40ca
Jürgen Gmach <juergen.gmach@canonical.com>
2021-11-16 10:00:42 +0100

    Fix pymemcache regression in SnapStoreClient.listChannels
    The new JsonClient has two new methods:
    - get_json
    - set_json
    
    `get_json` gracefully handles invalid data by deleting the key, so the
    value gets calculated and stored again.
    
    This enables a smooth transition as the previously used library saved
    binary data.
    
    LP: #1950811

--- lib/lp/services/memcache/client.py
+++ lib/lp/services/memcache/client.py
@@ -4,9 +4,11 @@
 """Launchpad Memcache client."""
 
 __all__ = [
+    'MemcacheClient',
     'memcache_client_factory',
     ]
 
+import json
 import re
 
 from pymemcache.client.hash import HashClient
@@ -18,8 +20,48 @@ from pymemcache.serde import (
 from lp.services.config import config
 
 
+class MemcacheClient(HashClient):
+    """memcached client with added JSON handling"""
+
+    def get_json(self, key, logger, description, default=None):
+        """Returns decoded JSON data from a memcache instance for a given key
+
+        In case of a decoding issue, and given a logger and a description, an
+        error message gets logged.
+
+        The `default` value is used when no value could be retrieved, or an
+        error happens.
+
+        :returns: dict or the default value
+        """
+        data = self.get(key)
+        if data is not None:
+            try:
+                rv = json.loads(data)
+            # the exceptions are chosen deliberately in order to gracefully
+            # handle invalid data
+            except (TypeError, ValueError):
+                if logger and description:
+                    logger.exception(
+                        "Cannot load cached %s; deleting" % description
+                    )
+                self.delete(key)
+                rv = default
+        else:
+            rv = default
+        return rv
+
+    def set_json(self, key, value, expire=0):
+        """Saves the given key/value pair, after converting the value.
+
+        `expire` (optional int) is the number of seconds until the item
+            expires from the cache; zero means no expiry.
+        """
+        self.set(key, json.dumps(value), expire)
+
+
 def memcache_client_factory(timeline=True):
-    """Return a pymemcache HashClient for Launchpad."""
+    """Return an extended pymemcache client for Launchpad."""
     # example value for config.memcache.servers:
     # (127.0.0.1:11242,1)
     servers = [
@@ -31,7 +73,7 @@ def memcache_client_factory(timeline=True):
         from lp.services.memcache.timeline import TimelineRecordingClient
         client_factory = TimelineRecordingClient
     else:
-        client_factory = HashClient
+        client_factory = MemcacheClient
     return client_factory(
         servers,
         serializer=python_memcache_serializer,
