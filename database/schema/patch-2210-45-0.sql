6cf168ea88e048a8e2e31ff3326780a5524895d7
Ioana Lasc <ioana.lasc@canonical.com>
2022-03-22 12:33:41 +0200

    Default date_created to NOT NULL on RevisionStatusArtifact

--- database/schema/patch-2210-45-0.sql
+++ database/schema/patch-2210-45-0.sql
@@ -4,6 +4,15 @@
 SET client_min_messages=ERROR;
 
 ALTER TABLE RevisionStatusArtifact
-    ADD COLUMN date_created timestamp without time zone;
+    ADD COLUMN date_created timestamp without time zone DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL;
+
+UPDATE RevisionStatusArtifact
+SET date_created = LibraryFileAlias.date_created
+FROM LibraryFileAlias
+WHERE RevisionStatusArtifact.library_file = LibraryFileAlias.id;
+
+CREATE INDEX revisionstatusartifact__report__type__created__idx
+    ON RevisionStatusArtifact (report, type, date_created DESC);
+DROP INDEX revisionstatusartifact__report__type__idx;
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2210, 45, 0);
