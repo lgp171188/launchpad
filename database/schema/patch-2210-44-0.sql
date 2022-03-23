cb69f5b466d0f6376efa626423f31e0947002a1e
Colin Watson <cjwatson@canonical.com>
2022-03-23 15:47:34 +0000

    Avoid some slow operations in the initial cold patch

--- database/schema/patch-2210-44-0.sql
+++ database/schema/patch-2210-44-0.sql
@@ -6,26 +6,27 @@ SET client_min_messages=ERROR;
 -- STEP 1, COLD
 
 ALTER TABLE Archive
-    -- 0 == LOCAL
-    ADD COLUMN publishing_method integer DEFAULT 0,
-    -- 0 == DEBIAN
-    ADD COLUMN index_format integer DEFAULT 0;
+    ADD COLUMN publishing_method integer,
+    ADD COLUMN index_format integer;
 
 ALTER TABLE SourcePackageRelease
     ADD COLUMN ci_build integer REFERENCES CIBuild,
-    ADD CONSTRAINT at_most_one_build CHECK (
-        null_count(ARRAY[sourcepackage_recipe_build, ci_build]) >= 1),
+    ADD CONSTRAINT at_most_one_build
+        CHECK (null_count(ARRAY[sourcepackage_recipe_build, ci_build]) >= 1)
+        NOT VALID,
     ALTER COLUMN component DROP NOT NULL,
     ALTER COLUMN section DROP NOT NULL,
     ALTER COLUMN urgency DROP NOT NULL,
     ALTER COLUMN dsc_format DROP NOT NULL,
-    ADD CONSTRAINT debian_columns CHECK (
-        -- 1 == DPKG
-        format != 1
-        OR (component IS NOT NULL
-            AND section IS NOT NULL
-            AND urgency IS NOT NULL
-            AND dsc_format IS NOT NULL)),
+    ADD CONSTRAINT debian_columns
+        CHECK (
+            -- 1 == DPKG
+            format != 1
+            OR (component IS NOT NULL
+                AND section IS NOT NULL
+                AND urgency IS NOT NULL
+                AND dsc_format IS NOT NULL))
+        NOT VALID,
     -- This is always non-NULL for Debian-format source packages, but it's
     -- not particularly important to constrain this at the DB level.
     ALTER COLUMN maintainer DROP NOT NULL;
@@ -39,35 +40,41 @@ ALTER TABLE SourcePackagePublishingHistory
             -- 1 == DPKG
             AND format != 1)
         OR (component IS NOT NULL
-            AND section IS NOT NULL)),
+            AND section IS NOT NULL)) NOT VALID,
     ADD COLUMN channel jsonb;
 
 ALTER TABLE BinaryPackageRelease
     ADD COLUMN ci_build integer REFERENCES CIBuild,
     ALTER COLUMN build DROP NOT NULL,
-    ADD CONSTRAINT one_build CHECK (null_count(ARRAY[build, ci_build]) = 1),
+    ADD CONSTRAINT one_build
+        CHECK (null_count(ARRAY[build, ci_build]) = 1)
+        NOT VALID,
     ALTER COLUMN component DROP NOT NULL,
     ALTER COLUMN section DROP NOT NULL,
     ALTER COLUMN priority DROP NOT NULL,
-    ADD CONSTRAINT debian_columns CHECK (
-        -- 1 == DEB, 2 == UDEB, 5 == DDEB
-        binpackageformat NOT IN (1, 2, 5)
-        OR (component IS NOT NULL
-            AND section IS NOT NULL
-            AND priority IS NOT NULL));
+    ADD CONSTRAINT debian_columns
+        CHECK (
+            -- 1 == DEB, 2 == UDEB, 5 == DDEB
+            binpackageformat NOT IN (1, 2, 5)
+            OR (component IS NOT NULL
+                AND section IS NOT NULL
+                AND priority IS NOT NULL))
+        NOT VALID;
 
 ALTER TABLE BinaryPackagePublishingHistory
     ADD COLUMN binarypackageformat integer,
     ALTER COLUMN component DROP NOT NULL,
     ALTER COLUMN section DROP NOT NULL,
     ALTER COLUMN priority DROP NOT NULL,
-    ADD CONSTRAINT debian_columns CHECK (
-        (binarypackageformat IS NOT NULL
-            -- 1 == DEB, 2 == UDEB, 5 == DDEB
-            AND binarypackageformat NOT IN (1, 2, 5))
-        OR (component IS NOT NULL
-            AND section IS NOT NULL
-            AND priority IS NOT NULL)),
+    ADD CONSTRAINT debian_columns
+        CHECK (
+            (binarypackageformat IS NOT NULL
+                -- 1 == DEB, 2 == UDEB, 5 == DDEB
+                AND binarypackageformat NOT IN (1, 2, 5))
+            OR (component IS NOT NULL
+                AND section IS NOT NULL
+                AND priority IS NOT NULL))
+        NOT VALID,
     ADD COLUMN channel jsonb;
 
 
@@ -79,8 +86,14 @@ ALTER TABLE BinaryPackagePublishingHistory
 CREATE INDEX sourcepackagerelease__ci_build__idx
     ON SourcePackageRelease (ci_build);
 
+ALTER TABLE SourcePackageRelease
+    VALIDATE CONSTRAINT at_most_one_build,
+    VALIDATE CONSTRAINT debian_columns;
+
 CREATE INDEX sourcepackagepublishinghistory__channel__idx
     ON SourcePackagePublishingHistory (channel);
+
+ALTER TABLE SourcePackagePublishingHistory VALIDATE CONSTRAINT debian_columns;
 
 CREATE UNIQUE INDEX binarypackagerelease__bpn__build__version__key
     ON BinaryPackageRelease (
@@ -90,8 +103,14 @@ CREATE UNIQUE INDEX binarypackagerelease__build__bpn__key
 CREATE INDEX binarypackagerelease__ci_build__idx
     ON BinaryPackageRelease (ci_build);
 
+ALTER TABLE BinaryPackageRelease
+    VALIDATE CONSTRAINT one_build,
+    VALIDATE CONSTRAINT debian_columns;
+
 CREATE INDEX binarypackagepublishinghistory__channel__idx
     ON BinaryPackagePublishingHistory (channel);
+
+ALTER TABLE BinaryPackagePublishingHistory VALIDATE CONSTRAINT debian_columns;
 
 
 -- STEP 3, COLD
