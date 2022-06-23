36762129197e82363f4ab5abf926df1d901f6003
Colin Watson <cjwatson@canonical.com>
2022-06-23 09:55:32 +0100

    Do the DROP INDEX cleanup step here as well
    We can do this concurrently, so it doesn't need to be a separate cold
    patch.

--- database/schema/patch-2210-44-4.sql
+++ database/schema/patch-2210-44-4.sql
@@ -6,5 +6,6 @@ SET client_min_messages=ERROR;
 CREATE UNIQUE INDEX binarypackagerelease__ci_build__bpn__format__key
     ON BinaryPackageRelease (ci_build, binarypackagename, binpackageformat)
     WHERE ci_build IS NOT NULL;
+DROP INDEX binarypackagerelease__ci_build__bpn__key;
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2210, 44, 4);
