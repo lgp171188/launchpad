d3c87a5be1201b03ea4ee63abd326b5fcc4977b8
Andrey Fedoseev <andrey.fedoseev@canonical.com>
2022-07-19 12:11:04 +0500

    Add `uct-import` script
    The script is meant to import CVE entries from `ubuntu-cve-tracker` to
    Launchpad database as bugs/bug tasks/vulnerabilities
    
    The script parses the CVE files using the code copied from
    `cve_lib` in `ubuntu-cve-tracker`, and transforms the data to
    structured `CVE` objects.
    
    The `CVE` object is then imported as a `Bug`, with `BugTask` created for:
    
    - Every package listed in the CVE
    - Every combination of package/distroseries listed in the CVE
    
    `Vulnerability` instance is created for every distribution affected by the CVE
    
    The `Bug` and the `Vulnerability`s are linked to the corresponding `Cve`
    object which is supposed to be present in LP database

---
+++ scripts/uct-import.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3 -S
+#
+# Copyright 2022 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+import _pythonpath  # noqa: F401
+
+import logging
+from pathlib import Path
+
+from lp.bugs.scripts.uctimport import UCTImporter
+from lp.services.scripts.base import LaunchpadScript
+
+
+class UCTImportScript(LaunchpadScript):
+
+    description = (
+        "Import bugs into Launchpad from CVE entries in ubuntu-cve-tracker."
+    )
+    loglevel = logging.INFO
+
+    def main(self):
+        if len(self.args) != 1:
+            self.parser.error('Please specify a CVE file to import')
+
+        importer = UCTImporter()
+
+        cve_path = Path(self.args[0])
+        importer.import_cve_from_file(cve_path)
+
+
+if __name__ == '__main__':
+    script = UCTImportScript('lp.services.scripts.uctimport')
+    script.run()
