58151c70c469aff6a0fc405f5407e17067af1023
Colin Watson <cjwatson@canonical.com>
2022-11-29 12:50:30 +0000

    Avoid unnecessary queries in BuilderResource vocabulary
    Using a `SimpleVocabulary` means we have to find the available terms
    before instantiating the vocabulary.  Reimplement this a bit more
    manually to avoid that problem.

--- lib/lp/buildmaster/tests/test_vocabularies.py
+++ lib/lp/buildmaster/tests/test_vocabularies.py
@@ -1,6 +1,7 @@
 # Copyright 2022 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
+from zope.schema.interfaces import ITerm, ITokenizedTerm, IVocabularyTokenized
 from zope.schema.vocabulary import getVocabularyRegistry
 
 from lp.testing import TestCaseWithFactory
@@ -10,6 +11,44 @@ from lp.testing.layers import ZopelessDatabaseLayer
 class TestBuilderResourceVocabulary(TestCaseWithFactory):
 
     layer = ZopelessDatabaseLayer
+
+    def test_provides_interface(self):
+        self.factory.makeBuilder()
+        repository = self.factory.makeGitRepository()
+        vocab = getVocabularyRegistry().get(repository, "BuilderResource")
+        self.assertProvides(vocab, IVocabularyTokenized)
+
+    def test___contains__(self):
+        self.factory.makeBuilder(open_resources=["large"])
+        repository = self.factory.makeGitRepository()
+        vocab = getVocabularyRegistry().get(repository, "BuilderResource")
+        self.assertIn("large", vocab)
+        self.assertNotIn("small", vocab)
+
+    def test___len__(self):
+        self.factory.makeBuilder(open_resources=["large", "larger"])
+        repository = self.factory.makeGitRepository()
+        vocab = getVocabularyRegistry().get(repository, "BuilderResource")
+        self.assertEqual(2, len(vocab))
+
+    def test_getTerm(self):
+        self.factory.makeBuilder(open_resources=["large"])
+        repository = self.factory.makeGitRepository()
+        vocab = getVocabularyRegistry().get(repository, "BuilderResource")
+        term = vocab.getTerm("large")
+        self.assertProvides(term, ITerm)
+        self.assertEqual("large", term.value)
+        self.assertRaises(LookupError, vocab.getTerm, "small")
+
+    def test_getTermByToken(self):
+        self.factory.makeBuilder(open_resources=["large"])
+        repository = self.factory.makeGitRepository()
+        vocab = getVocabularyRegistry().get(repository, "BuilderResource")
+        term = vocab.getTermByToken("large")
+        self.assertProvides(term, ITokenizedTerm)
+        self.assertEqual("large", term.value)
+        self.assertEqual("large", term.token)
+        self.assertRaises(LookupError, vocab.getTerm, "small")
 
     def test_no_resources(self):
         self.factory.makeBuilder()
--- lib/lp/buildmaster/vocabularies.py
+++ lib/lp/buildmaster/vocabularies.py
@@ -4,17 +4,20 @@
 """Soyuz vocabularies."""
 
 __all__ = [
-    "builder_resource_vocabulary_factory",
+    "BuilderResourceVocabulary",
     "ProcessorVocabulary",
 ]
 
 from storm.expr import Alias, Cast, Coalesce, Func
-from zope.schema.vocabulary import SimpleTerm, SimpleVocabulary
+from zope.interface import implementer
+from zope.schema.interfaces import IVocabularyTokenized
+from zope.schema.vocabulary import SimpleTerm
 
 from lp.buildmaster.model.builder import Builder
 from lp.buildmaster.model.processor import Processor
 from lp.services.database.interfaces import IStore
 from lp.services.database.stormexpr import Concatenate
+from lp.services.propertycache import cachedproperty
 from lp.services.webapp.vocabulary import NamedSQLObjectVocabulary
 
 
@@ -25,33 +28,70 @@ class ProcessorVocabulary(NamedSQLObjectVocabulary):
     _orderBy = "name"
 
 
-def builder_resource_vocabulary_factory(context):
-    """Return a vocabulary of all currently-defined builder resources.
+@implementer(IVocabularyTokenized)
+class BuilderResourceVocabulary:
+    """A vocabulary of all currently-defined builder resources.
 
     The context is anything with a `builder_constraints` attribute; the
     current constraints are merged into the set of available builder
     resources, to avoid problems if some unknown resources are already set
     as constraints.
     """
-    resources = set(
-        IStore(Builder)
-        .find(
-            Alias(
-                Func(
-                    "jsonb_array_elements_text",
-                    Concatenate(
-                        Coalesce(Builder.open_resources, Cast("[]", "jsonb")),
-                        Coalesce(
-                            Builder.restricted_resources, Cast("[]", "jsonb")
+
+    def __init__(self, context):
+        self.context = context
+
+    @cachedproperty
+    def _resources(self):
+        builder_resources = set(
+            IStore(Builder)
+            .find(
+                Alias(
+                    Func(
+                        "jsonb_array_elements_text",
+                        Concatenate(
+                            Coalesce(
+                                Builder.open_resources, Cast("[]", "jsonb")
+                            ),
+                            Coalesce(
+                                Builder.restricted_resources,
+                                Cast("[]", "jsonb"),
+                            ),
                         ),
                     ),
+                    "resource",
                 ),
-                "resource",
-            ),
-            Builder.active,
+                Builder.active,
+            )
+            .config(distinct=True)
         )
-        .config(distinct=True)
-    ).union(context.builder_constraints or set())
-    return SimpleVocabulary(
-        [SimpleTerm(resource) for resource in sorted(resources)]
-    )
+        return sorted(
+            builder_resources.union(self.context.builder_constraints or set())
+        )
+
+    def __contains__(self, value):
+        """See `zope.schema.interfaces.ISource`."""
+        return value in self._resources
+
+    def __iter__(self):
+        """See `zope.schema.interfaces.IIterableVocabulary`."""
+        for resource in self._resources:
+            yield SimpleTerm(resource)
+
+    def __len__(self):
+        """See `zope.schema.interfaces.IIterableVocabulary`."""
+        return len(self._resources)
+
+    def getTerm(self, value):
+        """See `zope.schema.interfaces.IBaseVocabulary`."""
+        if value in self._resources:
+            return SimpleTerm(value)
+        else:
+            raise LookupError(value)
+
+    def getTermByToken(self, token):
+        """See `zope.schema.interfaces.IVocabularyTokenized`."""
+        if token in self._resources:
+            return SimpleTerm(token)
+        else:
+            raise LookupError(token)
