9ba7fdfdea57839dc0b4fdbd4f5e3c2524a11356
Jürgen Gmach <juergen.gmach@canonical.com>
2023-01-20 17:26:36 +0100

    Enable filtering for UCTImports
    The CLI now takes a `--filter` option which takes a glob-style pattern
    to filter the CVEs for import.

---
+++ lib/lp/bugs/scripts/tests/test_uctimport.py
@@ -0,0 +1,88 @@
+from pathlib import Path
+
+from lp.testing import TestCase
+from lp.testing.layers import LaunchpadZopelessLayer
+from lp.testing.script import run_script
+
+
+class TestUCTImportScript(TestCase):
+    """Test the TestUCTImportScript class."""
+
+    layer = LaunchpadZopelessLayer
+
+    def test_no_path_given(self):
+        """TestUCTImportScript errors when no path given"""
+        exit_code, out, err = run_script(
+            script="scripts/uct-import.py",
+            args=[],
+        )
+        self.assertEqual(2, exit_code)
+        self.assertEqual("", out)
+        self.assertEqual(
+            "Usage: uct-import.py [options] PATH\n\nuct-import.py: "
+            "error: Please specify a path to import\n",
+            err,
+        )
+
+    def test_load_from_file(self):
+        load_from = Path(__file__).parent / "sampledata" / "CVE-2022-23222"
+        exit_code, out, err = run_script(
+            script="scripts/uct-import.py",
+            args=[str(load_from)],
+        )
+        self.assertEqual(0, exit_code)
+        self.assertEqual("", out)
+        self.assertIn("CVE-2022-23222 was imported successfully", err)
+
+    def test_load_from_directory(self):
+        load_from = Path(__file__).parent / "sampledata"
+        exit_code, out, err = run_script(
+            script="scripts/uct-import.py",
+            args=[str(load_from)],
+        )
+        self.assertEqual(0, exit_code)
+        self.assertEqual("", out)
+        self.assertIn("CVE-2007-0255 was imported successfully", err)
+        self.assertIn("CVE-2022-3219 was imported successfully", err)
+        self.assertIn("CVE-2022-23222 was imported successfully", err)
+
+    def test_dry_run_does_not_crash(self):
+        load_from = Path(__file__).parent / "sampledata" / "CVE-2022-23222"
+        exit_code, out, err = run_script(
+            script="scripts/uct-import.py",
+            args=[str(load_from), "--dry-run"],
+        )
+        self.assertEqual(0, exit_code)
+        self.assertEqual("", out)
+        self.assertRegex(err, r"^INFO    Importing.*CVE-2022-23222.*")
+
+    def test_filter_cve(self):
+        load_from = Path(__file__).parent / "sampledata"
+        exit_code, out, err = run_script(
+            script="scripts/uct-import.py",
+            args=[str(load_from), "--filter", "2007*"],
+        )
+        self.assertEqual(0, exit_code)
+        self.assertEqual("", out)
+        self.assertNotIn("CVE-2022-23222 was imported successfully", err)
+        self.assertIn("CVE-2007-0255 was imported successfully", err)
+
+        exit_code, out, err = run_script(
+            script="scripts/uct-import.py",
+            args=[str(load_from), "--filter", "2022*"],
+        )
+        self.assertEqual(0, exit_code)
+        self.assertEqual("", out)
+        self.assertIn("CVE-2022-23222 was imported successfully", err)
+        self.assertIn("CVE-2022-3219 was imported successfully", err)
+        self.assertNotIn("CVE-2007-0255 was imported successfully", err)
+
+        exit_code, out, err = run_script(
+            script="scripts/uct-import.py",
+            args=[str(load_from), "--filter", "20[02][27]*"],
+        )
+        self.assertEqual(0, exit_code)
+        self.assertEqual("", out)
+        self.assertIn("CVE-2022-23222 was imported successfully", err)
+        self.assertIn("CVE-2022-3219 was imported successfully", err)
+        self.assertIn("CVE-2007-0255 was imported successfully", err)
--- scripts/uct-import.py
+++ scripts/uct-import.py
@@ -4,59 +4,7 @@
 # GNU Affero General Public License version 3 (see the file LICENSE).
 import _pythonpath  # noqa: F401
 
-import logging
-from pathlib import Path
-
-from lp.app.validators.cve import CVEREF_PATTERN
-from lp.bugs.scripts.uct import UCTImporter
-from lp.services.scripts.base import LaunchpadScript
-
-logger = logging.getLogger(__name__)
-
-
-class UCTImportScript(LaunchpadScript):
-
-    usage = "usage: %prog [options] PATH"
-    description = (
-        "Import bugs into Launchpad from CVE entries in ubuntu-cve-tracker. "
-        "PATH is either path to a CVE file, or path to a directory "
-        "containing the CVE files"
-    )
-    loglevel = logging.INFO
-
-    def add_my_options(self):
-        self.parser.add_option(
-            "--dry-run",
-            action="store_true",
-            dest="dry_run",
-            default=False,
-            help="Don't commit changes to the DB.",
-        )
-
-    def main(self):
-        if len(self.args) != 1:
-            self.parser.error("Please specify a path to import")
-
-        path = Path(self.args[0])
-        if path.is_dir():
-            logger.info(
-                "Importing CVE files from directory: %s", path.resolve()
-            )
-            cve_paths = sorted(
-                p
-                for p in path.rglob("CVE-*")
-                if p.is_file() and CVEREF_PATTERN.match(p.name)
-            )
-            if not cve_paths:
-                logger.warning("Could not find CVE files in %s", path)
-                return
-        else:
-            cve_paths = [path]
-
-        importer = UCTImporter(dry_run=self.options.dry_run)
-        for cve_path in cve_paths:
-            importer.import_cve_from_file(cve_path)
-
+from lp.bugs.scripts.uctimport import UCTImportScript
 
 if __name__ == "__main__":
     script = UCTImportScript("lp.services.scripts.uctimport")
