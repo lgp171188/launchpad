43dd651ea33c4cd88cc6c39022b21baa4ec0a86e
Colin Watson <cjwatson@canonical.com>
2023-03-16 09:11:34 +0000

    charm: Add a simple launchpad-admin charm
    This is useful as a way to get hold of database superuser credentials in
    a charmed deployment, without having to SSH into a `postgresql` unit
    directly.
    
    For now this is just for interactive use, but I can think of a couple of
    natural future extensions: we could do with an action to initialize a
    new Launchpad database, and it might well make sense to use this charm
    as a place to run our schema upgrade machinery.

---
+++ charm/launchpad-admin/reactive/launchpad-admin.py
@@ -0,0 +1,71 @@
+# Copyright 2023 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+import os.path
+
+from charmhelpers.core import hookenv, host, templating
+from charms.launchpad.base import (
+    configure_lazr,
+    get_service_config,
+    home_dir,
+    strip_dsn_authentication,
+    update_pgpass,
+)
+from charms.reactive import set_state, when, when_not
+from ols import base, postgres
+from psycopg2.extensions import make_dsn, parse_dsn
+
+
+def strip_password(dsn):
+    parsed_dsn = parse_dsn(dsn)
+    parsed_dsn.pop("password", None)
+    return make_dsn(**parsed_dsn)
+
+
+@when(
+    "launchpad.base.configured",
+    "db.master.available",
+    "db-admin.master.available",
+    "session-db.master.available",
+)
+@when_not("service_configured")
+def configure(db, db_admin, session_db):
+    config = get_service_config()
+    db_primary, _ = postgres.get_db_uris(db)
+    db_admin_primary, _ = postgres.get_db_uris(db_admin)
+    session_db_primary, _ = postgres.get_db_uris(session_db)
+    update_pgpass(db_admin_primary)
+    update_pgpass(session_db_primary)
+    config["db_primary"] = strip_password(db_primary)
+    config["db_admin_primary"] = strip_password(db_admin_primary)
+    config["db_session_primary"] = strip_password(session_db_primary)
+    config["db_session"] = strip_dsn_authentication(session_db_primary)
+    config["db_session_user"] = parse_dsn(session_db_primary)["user"]
+    configure_lazr(
+        config,
+        "launchpad-admin-lazr.conf",
+        "launchpad-admin/launchpad-lazr.conf",
+    )
+    templating.render(
+        "bash_aliases.j2",
+        os.path.join(home_dir(), ".bash_aliases"),
+        config,
+        owner=base.user(),
+        group=base.user(),
+        perms=0o644,
+    )
+    bin_dir = os.path.join(home_dir(), "bin")
+    host.mkdir(bin_dir, owner=base.user(), group=base.user(), perms=0o755)
+    for script in ("db", "db-admin", "db-session"):
+        script_path = os.path.join(bin_dir, script)
+        templating.render(
+            f"{script}.j2",
+            script_path,
+            config,
+            owner=base.user(),
+            group=base.user(),
+            perms=0o755,
+        )
+
+    set_state("service.configured")
+    hookenv.status_set("active", "Ready")
