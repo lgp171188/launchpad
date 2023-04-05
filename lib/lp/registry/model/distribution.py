lgp171188/launchpad

commit 559bbefbb5d7327debd5343c1e812787f5d0caad

Retain more ArchiveFile history
Source and binary packages are removed from the published archive on
disk a day after they are no longer referenced by current index files;
package files in the librarian are typically expired a week after all
their associated publishing history rows have been marked as removed
from disk, although files in some archives (primary archives and certain
named PPAs) never expire.

Archive index files, such as `Packages`, `Sources`, and `Release` files,
currently work quite differently: old versions of these files are
published in `by-hash` directories on disk so that clients can continue
to make use of them for a grace period of a day, after which they are
both removed from disk and removed from the database.

The latter approach is problematic for the forthcoming snapshot service,
because it means we can only serve snapshots of index files for as long
as they are still published on disk: not much more useful than what we
already have!

This commit implements a much more useful policy.  We now treat archive
index files similarly to source and binary packages: they're removed
from disk after a day as before, to prevent `by-hash` directories
getting unreasonably large and causing bloat on mirrors, but now we
merely set the `ArchiveFile.date_removed` column rather than deleting
the row entirely.  The `expire-archive-files` script now handles
`ArchiveFile` as well as `SourcePackageReleaseFile` and
`BinaryPackageFile` rows, which means that old snapshots of the primary
archive and some other important archives will be retained in the
database and the librarian.

This will of course cause the librarian to grow more quickly, but some
back-of-the-envelope calculations suggest that even if we retain old
archive index files in perpetuity then this will be on the order of an
extra terabyte per year, which seems quite manageable.

lib/lp/soyuz/model/archivefile.py
@@ -12,7 +12,7 @@ import os.path
 import re
 
 import pytz
-from storm.locals import And, DateTime, Int, Reference, Unicode
+from storm.locals import DateTime, Int, Reference, Unicode
 from zope.component import getUtility
 from zope.interface import implementer
 
@@ -20,7 +20,6 @@ from lp.services.database.bulk import load_related
 from lp.services.database.constants import UTC_NOW
 from lp.services.database.decoratedresultset import DecoratedResultSet
 from lp.services.database.interfaces import IPrimaryStore, IStore
-from lp.services.database.sqlbase import convert_storm_clause_to_string
 from lp.services.database.stormbase import StormBase
 from lp.services.database.stormexpr import RegexpMatch
 from lp.services.librarian.interfaces import ILibraryFileAliasSet
@@ -71,6 +70,10 @@ class ArchiveFile(StormBase):
         name="scheduled_deletion_date", tzinfo=pytz.UTC, allow_none=True
     )
 
+    date_removed = DateTime(
+        name="date_removed", tzinfo=pytz.UTC, allow_none=True
+    )
+
     def __init__(self, archive, container, path, library_file):
         """Construct an `ArchiveFile`."""
         super().__init__()
@@ -81,6 +84,7 @@ class ArchiveFile(StormBase):
         self.date_created = _now()
         self.date_superseded = None
         self.scheduled_deletion_date = None
+        self.date_removed = None
 
 
 @implementer(IArchiveFileSet)
@@ -116,6 +120,7 @@ class ArchiveFileSet:
         path_parent=None,
         sha256=None,
         condemned=None,
+        only_published=False,
         eager_load=False,
     ):
         """See `IArchiveFileSet`."""
@@ -145,6 +150,8 @@ class ArchiveFileSet:
                 clauses.append(ArchiveFile.scheduled_deletion_date != None)
             else:
                 clauses.append(ArchiveFile.scheduled_deletion_date == None)
+        if only_published:
+            clauses.append(ArchiveFile.date_removed == None)
         archive_files = IStore(ArchiveFile).find(ArchiveFile, *clauses)
 
         def eager_load(rows):
@@ -185,30 +192,13 @@ class ArchiveFileSet:
         )
 
     @staticmethod
-    def delete(archive_files):
+    def markDeleted(archive_files):
         """See `IArchiveFileSet`."""
-        # XXX cjwatson 2016-03-30 bug=322972: Requires manual SQL due to
-        # lack of support for DELETE FROM ... USING ... in Storm.
-        clauses = [
+        rows = IPrimaryStore(ArchiveFile).find(
+            ArchiveFile,
             ArchiveFile.id.is_in(
                 {archive_file.id for archive_file in archive_files}
             ),
-            ArchiveFile.library_file_id == LibraryFileAlias.id,
-            LibraryFileAlias.contentID == LibraryFileContent.id,
-        ]
-        where = convert_storm_clause_to_string(And(*clauses))
-        return list(
-            IPrimaryStore(ArchiveFile).execute(
-                """
-            DELETE FROM ArchiveFile
-            USING LibraryFileAlias, LibraryFileContent
-            WHERE """
-                + where
-                + """
-            RETURNING
-                ArchiveFile.container,
-                ArchiveFile.path,
-                LibraryFileContent.sha256
-            """
-            )
+            ArchiveFile.date_removed == None,
         )
+        rows.set(date_removed=_now())

lib/lp/archivepublisher/tests/test_publisher.py
@@ -17,6 +17,7 @@ from datetime import datetime, timedelta
 from fnmatch import fnmatch
 from functools import partial
 from textwrap import dedent
+from typing import Optional, Sequence, Tuple
 from unittest import mock
 
 import pytz
@@ -3066,7 +3067,24 @@ class TestUpdateByHash(TestPublisherBase):
                 superseded_at + timedelta(days=BY_HASH_STAY_OF_EXECUTION)
             )
 
-    def assertHasSuiteFiles(self, patterns, *properties):
+    def assertHasSuiteFiles(
+        self,
+        patterns: Sequence[str],
+        *properties: Tuple[str, Optional[int], Optional[int], Optional[int]]
+    ) -> None:
+        """Assert that the database records certain archive files.
+
+        :param patterns: A sequence of `fnmatch` patterns for files under
+            `dists/breezy-autotest/` that we're interested in.
+        :param properties: A sequence of (`path`, `created_at`,
+            `superseded_at`, `removed_at`) tuples.  Each must match one of
+            the `ArchiveFile` rows matching `patterns`; `path` is relative
+            to `dists/breezy-autotest/`, while the `*_at` properties are
+            either None (indicating that the corresponding `date_*` column
+            should be None) or an index into `self.times` (indicating that
+            the corresponding `date_*` column should equal that time).
+        """
+
         def is_interesting(path):
             return any(
                 fnmatch(path, "dists/breezy-autotest/%s" % pattern)
@@ -3081,7 +3099,12 @@ class TestUpdateByHash(TestPublisherBase):
             if is_interesting(archive_file.path)
         ]
         matchers = []
-        for path, created_at, superseded_at in properties:
+        for path, created_at, superseded_at, removed_at in properties:
+            created_at = None if created_at is None else self.times[created_at]
+            superseded_at = (
+                None if superseded_at is None else self.times[superseded_at]
+            )
+            removed_at = None if removed_at is None else self.times[removed_at]
             scheduled_deletion_date_matcher = (
                 self._makeScheduledDeletionDateMatcher(superseded_at)
             )
@@ -3091,6 +3114,7 @@ class TestUpdateByHash(TestPublisherBase):
                     date_created=Equals(created_at),
                     date_superseded=Equals(superseded_at),
                     scheduled_deletion_date=scheduled_deletion_date_matcher,
+                    date_removed=Equals(removed_at),
                 )
             )
         self.assertThat(files, MatchesSetwise(*matchers))
@@ -3280,8 +3304,8 @@ class TestUpdateByHash(TestPublisherBase):
         flush_database_caches()
         self.assertHasSuiteFiles(
             ("Contents-*", "Release"),
-            ("Contents-i386", self.times[0], None),
-            ("Release", self.times[0], None),
+            ("Contents-i386", 0, None, None),
+            ("Release", 0, None, None),
         )
         releases = [get_release_contents()]
         self.assertThat(
@@ -3297,10 +3321,10 @@ class TestUpdateByHash(TestPublisherBase):
         flush_database_caches()
         self.assertHasSuiteFiles(
             ("Contents-*", "Release"),
-            ("Contents-i386", self.times[0], None),
-            ("Contents-hppa", self.times[1], None),
-            ("Release", self.times[0], self.times[1]),
-            ("Release", self.times[1], None),
+            ("Contents-i386", 0, None, None),
+            ("Contents-hppa", 1, None, None),
+            ("Release", 0, 1, None),
+            ("Release", 1, None, None),
         )
         releases.append(get_release_contents())
         self.assertThat(
@@ -3315,11 +3339,11 @@ class TestUpdateByHash(TestPublisherBase):
         flush_database_caches()
         self.assertHasSuiteFiles(
             ("Contents-*", "Release"),
-            ("Contents-i386", self.times[0], self.times[2]),
-            ("Contents-hppa", self.times[1], None),
-            ("Release", self.times[0], self.times[1]),
-            ("Release", self.times[1], self.times[2]),
-            ("Release", self.times[2], None),
+            ("Contents-i386", 0, 2, None),
+            ("Contents-hppa", 1, None, None),
+            ("Release", 0, 1, None),
+            ("Release", 1, 2, None),
+            ("Release", 2, None, None),
         )
         releases.append(get_release_contents())
         self.assertThat(
@@ -3333,12 +3357,12 @@ class TestUpdateByHash(TestPublisherBase):
         flush_database_caches()
         self.assertHasSuiteFiles(
             ("Contents-*", "Release"),
-            ("Contents-i386", self.times[0], self.times[2]),
-            ("Contents-hppa", self.times[1], None),
-            ("Release", self.times[0], self.times[1]),
-            ("Release", self.times[1], self.times[2]),
-            ("Release", self.times[2], self.times[3]),
-            ("Release", self.times[3], None),
+            ("Contents-i386", 0, 2, None),
+            ("Contents-hppa", 1, None, None),
+            ("Release", 0, 1, None),
+            ("Release", 1, 2, None),
+            ("Release", 2, 3, None),
+            ("Release", 3, None, None),
         )
         releases.append(get_release_contents())
         self.assertThat(
@@ -3365,10 +3389,13 @@ class TestUpdateByHash(TestPublisherBase):
         flush_database_caches()
         self.assertHasSuiteFiles(
             ("Contents-*", "Release"),
-            ("Contents-hppa", self.times[1], self.times[4]),
-            ("Release", self.times[2], self.times[3]),
-            ("Release", self.times[3], self.times[4]),
-            ("Release", self.times[4], None),
+            ("Contents-i386", 0, 2, 4),
+            ("Contents-hppa", 1, 4, None),
+            ("Release", 0, 1, 4),
+            ("Release", 1, 2, 4),
+            ("Release", 2, 3, None),
+            ("Release", 3, 4, None),
+            ("Release", 4, None, None),
         )
         releases.append(get_release_contents())
         self.assertThat(
@@ -3393,8 +3420,14 @@ class TestUpdateByHash(TestPublisherBase):
         flush_database_caches()
         self.assertHasSuiteFiles(
             ("Contents-*", "Release"),
-            ("Release", self.times[4], self.times[5]),
-            ("Release", self.times[5], None),
+            ("Contents-i386", 0, 2, 4),
+            ("Contents-hppa", 1, 4, 5),
+            ("Release", 0, 1, 4),
+            ("Release", 1, 2, 4),
+            ("Release", 2, 3, 5),
+            ("Release", 3, 4, 5),
+            ("Release", 4, 5, None),
+            ("Release", 5, None, None),
         )
         releases.append(get_release_contents())
         self.assertThat(suite_path("by-hash"), ByHashHasContents(releases[4:]))
@@ -3430,7 +3463,7 @@ class TestUpdateByHash(TestPublisherBase):
                 main_contents.add(f.read())
         self.assertHasSuiteFiles(
             ("main/source/Sources",),
-            ("main/source/Sources", self.times[0], None),
+            ("main/source/Sources", 0, None, None),
         )
 
         # Add a source package so that Sources is non-empty.
@@ -3447,8 +3480,8 @@ class TestUpdateByHash(TestPublisherBase):
         )
         self.assertHasSuiteFiles(
             ("main/source/Sources",),
-            ("main/source/Sources", self.times[0], self.times[1]),
-            ("main/source/Sources", self.times[1], None),
+            ("main/source/Sources", 0, 1, None),
+            ("main/source/Sources", 1, None, None),
         )
 
         # Delete the source package so that Sources is empty again.  The
@@ -3464,9 +3497,9 @@ class TestUpdateByHash(TestPublisherBase):
         )
         self.assertHasSuiteFiles(
             ("main/source/Sources",),
-            ("main/source/Sources", self.times[0], self.times[1]),
-            ("main/source/Sources", self.times[1], self.times[2]),
-            ("main/source/Sources", self.times[2], None),
+            ("main/source/Sources", 0, 1, None),
+            ("main/source/Sources", 1, 2, None),
+            ("main/source/Sources", 2, None, None),
         )
 
         # Make the first empty Sources file ready to prune.  This doesn't
@@ -3484,8 +3517,9 @@ class TestUpdateByHash(TestPublisherBase):
         )
         self.assertHasSuiteFiles(
             ("main/source/Sources",),
-            ("main/source/Sources", self.times[1], self.times[2]),
-            ("main/source/Sources", self.times[2], None),
+            ("main/source/Sources", 0, 1, 3),
+            ("main/source/Sources", 1, 2, None),
+            ("main/source/Sources", 2, None, None),
         )
 
     def setUpPruneableSuite(self):
@@ -3515,7 +3549,10 @@ class TestUpdateByHash(TestPublisherBase):
             for name in ("Release", "Sources.gz", "Sources.bz2"):
                 with open(suite_path("main", "source", name), "rb") as f:
                     main_contents.append(f.read())
-            self.advanceTime(delta=timedelta(hours=6))
+            # Advance time between each publisher run.  We don't advance
+            # time after the last one, since we'll do that below.
+            if sourcename != "baz":
+                self.advanceTime(delta=timedelta(hours=6))
         transaction.commit()
 
         # We have two condemned sets of index files and one uncondemned set.
@@ -3523,16 +3560,16 @@ class TestUpdateByHash(TestPublisherBase):
         # that it doesn't change.
         self.assertHasSuiteFiles(
             ("main/source/*", "Release"),
-            ("main/source/Sources.gz", self.times[0], self.times[1]),
-            ("main/source/Sources.gz", self.times[1], self.times[2]),
-            ("main/source/Sources.gz", self.times[2], None),
-            ("main/source/Sources.bz2", self.times[0], self.times[1]),
-            ("main/source/Sources.bz2", self.times[1], self.times[2]),
-            ("main/source/Sources.bz2", self.times[2], None),
-            ("main/source/Release", self.times[0], None),
-            ("Release", self.times[0], self.times[1]),
-            ("Release", self.times[1], self.times[2]),
-            ("Release", self.times[2], None),
+            ("main/source/Sources.gz", 0, 1, None),
+            ("main/source/Sources.gz", 1, 2, None),
+            ("main/source/Sources.gz", 2, None, None),
+            ("main/source/Sources.bz2", 0, 1, None),
+            ("main/source/Sources.bz2", 1, 2, None),
+            ("main/source/Sources.bz2", 2, None, None),
+            ("main/source/Release", 0, None, None),
+            ("Release", 0, 1, None),
+            ("Release", 1, 2, None),
+            ("Release", 2, None, None),
         )
         self.assertThat(suite_path("by-hash"), ByHashHasContents(top_contents))
         self.assertThat(
@@ -3574,13 +3611,16 @@ class TestUpdateByHash(TestPublisherBase):
         # generated.
         self.assertHasSuiteFiles(
             ("main/source/*", "Release"),
-            ("main/source/Sources.gz", self.times[1], self.times[2]),
-            ("main/source/Sources.gz", self.times[2], None),
-            ("main/source/Sources.bz2", self.times[1], self.times[2]),
-            ("main/source/Sources.bz2", self.times[2], None),
-            ("main/source/Release", self.times[0], None),
-            ("Release", self.times[1], self.times[2]),
-            ("Release", self.times[2], None),
+            ("main/source/Sources.gz", 0, 1, 3),
+            ("main/source/Sources.gz", 1, 2, None),
+            ("main/source/Sources.gz", 2, None, None),
+            ("main/source/Sources.bz2", 0, 1, 3),
+            ("main/source/Sources.bz2", 1, 2, None),
+            ("main/source/Sources.bz2", 2, None, None),
+            ("main/source/Release", 0, None, None),
+            ("Release", 0, 1, 3),
+            ("Release", 1, 2, None),
+            ("Release", 2, None, None),
         )
         self.assertThat(suite_path("by-hash"), ByHashHasContents(top_contents))
         self.assertThat(
@@ -3615,13 +3655,16 @@ class TestUpdateByHash(TestPublisherBase):
         # generated.
         self.assertHasSuiteFiles(
             ("main/source/*", "Release"),
-            ("main/source/Sources.gz", self.times[1], self.times[2]),
-            ("main/source/Sources.gz", self.times[2], None),
-            ("main/source/Sources.bz2", self.times[1], self.times[2]),
-            ("main/source/Sources.bz2", self.times[2], None),
-            ("main/source/Release", self.times[0], None),
-            ("Release", self.times[1], self.times[2]),
-            ("Release", self.times[2], None),
+            ("main/source/Sources.gz", 0, 1, 3),
+            ("main/source/Sources.gz", 1, 2, None),
+            ("main/source/Sources.gz", 2, None, None),
+            ("main/source/Sources.bz2", 0, 1, 3),
+            ("main/source/Sources.bz2", 1, 2, None),
+            ("main/source/Sources.bz2", 2, None, None),
+            ("main/source/Release", 0, None, None),
+            ("Release", 0, 1, 3),
+            ("Release", 1, 2, None),
+            ("Release", 2, None, None),
         )
         self.assertThat(suite_path("by-hash"), ByHashHasContents(top_contents))
         self.assertThat(

lib/lp/soyuz/tests/test_archivefile.py
@@ -251,22 +251,19 @@ class TestArchiveFile(TestCaseWithFactory):
             ),
         )
 
-    def test_delete(self):
+    def test_markDeleted(self):
         archive = self.factory.makeArchive()
         archive_files = [
             self.factory.makeArchiveFile(archive=archive) for _ in range(4)
         ]
-        expected_rows = [
-            (
-                archive_file.container,
-                archive_file.path,
-                archive_file.library_file.content.sha256,
-            )
-            for archive_file in archive_files[:2]
-        ]
         archive_file_set = getUtility(IArchiveFileSet)
-        rows = archive_file_set.delete(archive_files[:2])
-        self.assertContentEqual(expected_rows, rows)
+        archive_file_set.markDeleted(archive_files[:2])
+        flush_database_caches()
+        self.assertIsNotNone(archive_files[0].date_removed)
+        self.assertIsNotNone(archive_files[1].date_removed)
+        self.assertIsNone(archive_files[2].date_removed)
+        self.assertIsNone(archive_files[3].date_removed)
         self.assertContentEqual(
-            archive_files[2:], archive_file_set.getByArchive(archive)
+            archive_files[2:],
+            archive_file_set.getByArchive(archive, only_published=True),
         )

lib/lp/soyuz/interfaces/archivefile.py
@@ -72,6 +72,15 @@ class IArchiveFile(Interface):
         readonly=False,
     )
 
+    date_removed = Datetime(
+        title=_(
+            "The date when this file was entirely removed from the published "
+            "archive."
+        ),
+        required=False,
+        readonly=False,
+    )
+
 
 class IArchiveFileSet(Interface):
     """Bulk operations on files in an archive."""
@@ -104,6 +113,7 @@ class IArchiveFileSet(Interface):
         path=None,
         sha256=None,
         condemned=None,
+        only_published=False,
         eager_load=False,
     ):
         """Get files in an archive.
@@ -119,6 +129,8 @@ class IArchiveFileSet(Interface):
             scheduled_deletion_date set; if False, return only files without
             a scheduled_deletion_date set; if None (the default), return
             both.
+        :param only_published: If True, return only files without a
+            `date_removed` set.
         :param eager_load: If True, preload related `LibraryFileAlias` and
             `LibraryFileContent` rows.
         :return: An iterable of matched files.
@@ -141,10 +153,12 @@ class IArchiveFileSet(Interface):
         :return: An iterable of matched container names.
         """
 
-    def delete(archive_files):
-        """Delete these archive files.
+    def markDeleted(archive_files):
+        """Mark these archive files as deleted.
+
+        This does not actually delete the rows from the database;
+        `lp.soyuz.scripts.expire_archive_files` will expire their
+        corresponding `LibraryFileAlias` rows as needed.
 
         :param archive_files: The `IArchiveFile`s to delete.
-        :return: An iterable of (container, path, sha256) for files that
-            were deleted.
         """

database/schema/security.cfg
@@ -2032,6 +2032,7 @@ type=user
 [binaryfile-expire]
 groups=script
 public.archive                          = SELECT
+public.archivefile                      = SELECT
 public.binarypackagefile                = SELECT
 public.binarypackagepublishinghistory   = SELECT
 public.binarypackagerelease             = SELECT

lib/lp/archivepublisher/publishing.py
@@ -1274,17 +1274,19 @@ class Publisher:
         by_hashes = ByHashes(self._config.distsroot, self.log)
         existing_live_files = {}
         existing_nonlive_files = {}
-        keep_files = set()
         reapable_files = set()
 
         def strip_dists(path):
             assert path.startswith("dists/")
             return path[len("dists/") :]
 
-        # Record all files from the database.
+        # Record all published files from the database.
         db_now = get_transaction_timestamp(IStore(ArchiveFile))
         for db_file in archive_file_set.getByArchive(
-            self.archive, container=container, eager_load=True
+            self.archive,
+            container=container,
+            only_published=True,
+            eager_load=True,
         ):
             file_key = (
                 strip_dists(db_file.path),
@@ -1375,16 +1377,18 @@ class Publisher:
         # Remove any files from disk that aren't recorded in the database.
         by_hashes.prune()
 
-        # And remove expired ArchiveFiles from the DB now that we've pruned
-        # them and their directories from disk.
-        delete_files = reapable_files - keep_files
-        if delete_files:
-            for container, path, sha256 in archive_file_set.delete(
-                delete_files
-            ):
+        # And mark expired ArchiveFiles as deleted in the DB now that we've
+        # pruned them and their directories from disk.
+        if reapable_files:
+            archive_file_set.markDeleted(reapable_files)
+            for db_file in reapable_files:
                 self.log.debug(
-                    "by-hash: Deleted %s for %s in %s"
-                    % (sha256, path, container)
+                    "by-hash: Marked %s for %s in %s as deleted"
+                    % (
+                        db_file.library_file.content.sha256,
+                        db_file.path,
+                        db_file.container,
+                    )
                 )
 
     def _writeReleaseFile(self, suite, release_data):

commit 9f52289a5afa002e220ba887562f269a04e028ae

Don't consider removed ArchiveFiles as eligible for reaping
Commit 559bbefbb5d7327debd5343c1e812787f5d0caad introduced a gradual
performance regression for PPA publishing.  In order to support serving
old snapshots, `ArchiveFile` rows now have their `date_removed` column
set rather than being deleted from the database.  However, I failed to
account for this in the code that decides which PPAs to publish due to
pending deletions and which suites in each PPA should have `by-hash`
files pruned from them.  As a result, the PPA publisher has been getting
gradually slower as we accumulate more removed `ArchiveFile` rows.

Excluding removed rows makes the query for pending-publication PPAs
substantially faster since it has an order of magnitude fewer
`ArchiveFile` rows to sort, and it should mean that we spend less effort
publishing the same PPAs over and over again.

lib/lp/registry/model/distribution.py
@@ -1969,6 +1969,7 @@ class Distribution(
                 Archive.distribution == self,
                 ArchiveFile.archive == Archive.id,
                 ArchiveFile.scheduled_deletion_date < UTC_NOW,
+                ArchiveFile.date_removed == None,
             )
             .order_by(Archive.id)
             .config(distinct=True)

lib/lp/soyuz/model/archivefile.py
@@ -215,6 +215,7 @@ class ArchiveFileSet:
         clauses = [
             ArchiveFile.archive == archive,
             ArchiveFile.scheduled_deletion_date < _now(),
+            ArchiveFile.date_removed == None,
         ]
         if container_prefix is not None:
             clauses.append(ArchiveFile.container.startswith(container_prefix))

lib/lp/archivepublisher/tests/test_publisher.py
@@ -1499,6 +1499,8 @@ class TestPublisher(TestPublisherBase):
             archive_file
         ).scheduled_deletion_date = now - timedelta(hours=12)
         self.assertIn(archive, ubuntu.getPendingPublicationPPAs())
+        getUtility(IArchiveFileSet).markDeleted([archive_file])
+        self.assertNotIn(archive, ubuntu.getPendingPublicationPPAs())
 
     def testDirtySuitesArchive(self):
         # getPendingPublicationPPAs returns archives that have dirty_suites

lib/lp/soyuz/tests/test_archivefile.py
@@ -351,6 +351,10 @@ class TestArchiveFile(TestCaseWithFactory):
                 archive, container_prefix="release:"
             ),
         )
+        archive_file_set.markDeleted([archive_files[3]])
+        self.assertContentEqual(
+            ["release:foo"], archive_file_set.getContainersToReap(archive)
+        )
 
     def test_markDeleted(self):
         archive = self.factory.makeArchive()