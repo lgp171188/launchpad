c9074e2fe7cb311922147677045da9fcf051a152
Colin Watson <cjwatson@canonical.com>
2023-08-07 13:43:35 +0100

    Add adapters from various targets to IGitRepository
    Since `GitRefVocabulary` attempts to adapt the vocabulary context to
    `IGitRepository`, this fixes the problem where the git branch picker in
    the web UI only worked if the repository was given as the full unique
    name for a repository (e.g. `~cjwatson/launchpad/+git/launchpad`) rather
    than one of its shortened forms (e.g. `~cjwatson/launchpad`).

--- lib/lp/code/adapters/gitrepository.py
+++ lib/lp/code/adapters/gitrepository.py
@@ -4,16 +4,62 @@
 """Components and adapters related to Git repositories."""
 
 __all__ = [
+    "git_repository_for_distro_source_package",
+    "git_repository_for_oci_project",
+    "git_repository_for_person_distro_source_package",
+    "git_repository_for_person_oci_project",
+    "git_repository_for_person_product",
+    "git_repository_for_project",
     "GitRepositoryDelta",
 ]
 
 from lazr.lifecycle.objectdelta import ObjectDelta
+from zope.component import getUtility
 from zope.interface import implementer
 
 from lp.code.interfaces.gitrepository import (
     IGitRepository,
     IGitRepositoryDelta,
+    IGitRepositorySet,
 )
+
+
+def git_repository_for_project(project):
+    """Adapt a project to a Git repository."""
+    return getUtility(IGitRepositorySet).getDefaultRepository(project)
+
+
+def git_repository_for_distro_source_package(distro_source_package):
+    """Adapt a distribution source package to a Git repository."""
+    return getUtility(IGitRepositorySet).getDefaultRepository(
+        distro_source_package
+    )
+
+
+def git_repository_for_oci_project(oci_project):
+    """Adapt an OCI project to a Git repository."""
+    return getUtility(IGitRepositorySet).getDefaultRepository(oci_project)
+
+
+def git_repository_for_person_product(person_product):
+    """Adapt a PersonProduct to a Git repository."""
+    return getUtility(IGitRepositorySet).getDefaultRepositoryForOwner(
+        person_product.person, person_product.product
+    )
+
+
+def git_repository_for_person_distro_source_package(person_dsp):
+    """Adapt a PersonDistributionSourcePackage to a Git repository."""
+    return getUtility(IGitRepositorySet).getDefaultRepositoryForOwner(
+        person_dsp.person, person_dsp.distro_source_package
+    )
+
+
+def git_repository_for_person_oci_project(person_oci_project):
+    """Adapt a PersonOCIProject to a Git repository."""
+    return getUtility(IGitRepositorySet).getDefaultRepositoryForOwner(
+        person_oci_project.person, person_oci_project.oci_project
+    )
 
 
 @implementer(IGitRepositoryDelta)
