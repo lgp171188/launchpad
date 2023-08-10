7ca6419c387ea8fb4223042f0e46d19bf153cc26
Colin Watson <cjwatson@canonical.com>
2023-08-10 12:07:31 +0100

    Add RevisionStatusReport.distro_arch_series
    An awkward legacy of the order in which revision statuses and CI builds
    were designed is that revision status reports linked to a CI build don't
    record the series and architecture used for each individual CI build
    job.  This is a problem when we need to be able to do things like filter
    the artifacts produced by a CI build to select only those built on a
    particular series, and in general it seems like information that would
    be worth tracking in the database.  Add a column for it.

---
+++ database/schema/patch-2211-21-0.sql
@@ -0,0 +1,11 @@
+-- Copyright 2023 Canonical Ltd.  This software is licensed under the
+-- GNU Affero General Public License version 3 (see the file LICENSE).
+
+SET client_min_messages=ERROR;
+
+ALTER TABLE RevisionStatusReport
+    ADD COLUMN distro_arch_series integer REFERENCES distroarchseries;
+
+COMMENT ON COLUMN RevisionStatusReport.distro_arch_series IS 'The series and architecture for the CI build job that produced this report.';
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2211, 21, 0);
