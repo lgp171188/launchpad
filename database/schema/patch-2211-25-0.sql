351add36a8142fed801e9294d1ab75c22d87f976
Ines Almeida <ines.almeida@canonical.com>
2023-12-13 16:29:28 +0100

    db: rename column from 'identifier' to 'identity'

--- database/schema/patch-2211-25-0.sql
+++ database/schema/patch-2211-25-0.sql
@@ -7,12 +7,12 @@ CREATE TABLE SocialAccount (
     id serial PRIMARY KEY,
     person integer REFERENCES Person NOT NULL,
     platform integer NOT NULL,
-    identifier jsonb NOT NULL
+    identity jsonb NOT NULL
 );
 
 COMMENT ON COLUMN SocialAccount.person IS 'Person the social media account belongs to.';
 COMMENT ON COLUMN SocialAccount.platform IS 'Social media platform.';
-COMMENT ON COLUMN SocialAccount.identifier IS 'Identifier for the social media account (JSON format specific per social media platform).';
+COMMENT ON COLUMN SocialAccount.identity IS 'Data for identifying the social media account (JSON format specific per social media platform).';
 
 CREATE INDEX socialaccount__person__idx ON SocialAccount (person);
 CREATE INDEX socialaccount__platform__idx ON SocialAccount (platform);
