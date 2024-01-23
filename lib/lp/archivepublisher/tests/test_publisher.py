lgp171188/launchpad

commit 5f90943b2f5871b9e8a554e31c2361394a449dba

soyuz: Add toggle to turn off I18n/Index publishing
Signed-off-by: Dimitri John Ledkov <dimitri.ledkov@canonical.com>

lib/lp/archivepublisher/publishing.py
@@ -1682,11 +1682,12 @@ class Publisher:
             # Schedule i18n files for inclusion in the Release file.
             all_series_files.add(os.path.join(i18n_subpath, i18n_file))
 
-        with open(os.path.join(i18n_dir, "Index"), "wb") as f:
-            i18n_index.dump(f, "utf-8")
+        if distroseries.publish_i18n_index:
+            with open(os.path.join(i18n_dir, "Index"), "wb") as f:
+                i18n_index.dump(f, "utf-8")
 
-        # Schedule this for inclusion in the Release file.
-        all_series_files.add(os.path.join(component, "i18n", "Index"))
+            # Schedule this for inclusion in the Release file.
+            all_series_files.add(os.path.join(component, "i18n", "Index"))
 
     def _readIndexFileHashes(
         self, suite, file_name, subpath=None, real_file_name=None

lib/lp/archivepublisher/tests/test_publisher.py
@@ -2847,6 +2847,29 @@ class TestPublisher(TestPublisherBase):
 
         self.assertFalse(os.path.exists(os.path.join(i18n_root, "Index")))
 
+    def testWriteSuiteI18nPublishI18nFalse(self):
+        """i18n/Index is not generated when publish_i18n_index is False."""
+        publisher = Publisher(
+            self.logger,
+            self.config,
+            self.disk_pool,
+            self.ubuntutest.main_archive,
+        )
+        i18n_root = os.path.join(
+            self.config.distsroot, "breezy-autotest", "main", "i18n"
+        )
+
+        self.ubuntu["breezy-autotest"].publish_i18n_index = False
+
+        publisher._writeSuiteI18n(
+            self.ubuntutest["breezy-autotest"],
+            PackagePublishingPocket.RELEASE,
+            "main",
+            set(),
+        )
+
+        self.assertFalse(os.path.exists(os.path.join(i18n_root, "Index")))
+
     def testReadIndexFileHashesCompression(self):
         """Test compressed file handling in _readIndexFileHashes."""
         publisher = Publisher(

lib/lp/registry/configure.zcml
@@ -332,6 +332,7 @@
                 include_long_descriptions
                 index_compressors
                 publish_by_hash
+                publish_i18n_index
                 advertise_by_hash
                 strict_supported_component_dependencies
                 inherit_overrides_from_parents"/>

lib/lp/registry/interfaces/distroseries.py
@@ -585,6 +585,17 @@ class IDistroSeriesPublic(
         as_of="devel",
     )
 
+    publish_i18n_index = exported(
+        Bool(
+            title=_("Publish I18n index"),
+            required=True,
+            description=_(
+                """
+            Publish archive i18n/Index file, which is believed to be used."""
+            ),
+        )
+    )
+
     inherit_overrides_from_parents = Bool(
         title=_("Inherit overrides from parents"),
         readonly=False,

lib/lp/registry/model/distroseries.py
@@ -285,6 +285,7 @@ class DistroSeries(
             "publish_by_hash": False,
             "advertise_by_hash": False,
             "strict_supported_component_dependencies": True,
+            "publish_i18n_index": True,
         }
 
     @property
@@ -968,6 +969,15 @@ class DistroSeries(
         self.publishing_options[
             "strict_supported_component_dependencies"
         ] = value
+
+    @property
+    def publish_i18n_index(self):
+        return self.publishing_options.get("publish_i18n_index", True)
+
+    @publish_i18n_index.setter
+    def publish_i18n_index(self, value):
+        assert isinstance(value, bool)
+        self.publishing_options["publish_i18n_index"] = value
 
     def _customizeSearchParams(self, search_params):
         """Customize `search_params` for this distribution series."""

lib/lp/registry/tests/test_distroseries.py
@@ -448,6 +448,17 @@ class TestDistroSeries(TestCaseWithFactory):
             ]
         )
 
+    def test_publish_i18n_index(self):
+        distroseries = self.factory.makeDistroSeries()
+        self.assertTrue(distroseries.publish_i18n_index)
+        with admin_logged_in():
+            distroseries.publish_i18n_index = False
+        self.assertFalse(distroseries.publish_i18n_index)
+        naked_distroseries = removeSecurityProxy(distroseries)
+        self.assertFalse(
+            naked_distroseries.publishing_options["publish_i18n_index"]
+        )
+
 
 class TestDistroSeriesPackaging(TestCaseWithFactory):
     layer = DatabaseFunctionalLayer

lib/lp/soyuz/scripts/initialize_distroseries.py
@@ -454,6 +454,9 @@ class InitializeDistroSeries:
             parent.strict_supported_component_dependencies
             for parent in self.derivation_parents
         )
+        self.distroseries.publish_i18n_index = any(
+            parent.publish_i18n_index for parent in self.derivation_parents
+        )
 
     def _copy_architectures(self):
         log.info("Copying distroarchseries from parents.")

lib/lp/soyuz/scripts/tests/test_initialize_distroseries.py
@@ -96,6 +96,7 @@ class InitializationHelperTestCase(TestCaseWithFactory):
         parent.publish_by_hash = True
         parent.advertise_by_hash = True
         parent.strict_supported_component_dependencies = False
+        parent.publish_i18n_index = False
         self._populate_parent(parent, parent_das, packages, pocket)
         return parent, parent_das
 
@@ -842,6 +843,7 @@ class TestInitializeDistroSeries(InitializationHelperTestCase):
         self.assertTrue(child.publish_by_hash)
         self.assertTrue(child.advertise_by_hash)
         self.assertFalse(child.strict_supported_component_dependencies)
+        self.assertFalse(child.publish_i18n_index)
 
     def test_initialize(self):
         # Test a full initialize with no errors.

commit 2172d477047815bc9d59e14f4f592b3d171654ee

soyuz: fix up removal of stale i18n/Index upon removal
During qastaging testing it was discovered that i18n/Index publishing
correctly honors publish_i18n_index setting for freshly created suites
in an archive. But existing archives incorrectly left stale i18n/Index
on disk (whilst scheduling byhash symlink removal).

QA Testing:
* create a new archive on qastaging
* publish it once
* verify i18n/Index is published with byhash link to it
* toggle publish_i18n_index off
* publish it again
* verify i18n/Index is gone straight away
* verify deathrow 24h later removed byhash link to it as well

Improve unit test to correctly setup status quo publication, and
assert removal of the i18n/Index file upon change of the distroseries
publication setting.

Fixes: 5f90943b2f ("soyuz: Add toggle to turn off I18n/Index publishing")
Signed-off-by: Dimitri John Ledkov <dimitri.ledkov@canonical.com>

lib/lp/archivepublisher/publishing.py
@@ -1682,12 +1682,16 @@ class Publisher:
             # Schedule i18n files for inclusion in the Release file.
             all_series_files.add(os.path.join(i18n_subpath, i18n_file))
 
+        i18n_file_name = os.path.join(i18n_dir, "Index")
         if distroseries.publish_i18n_index:
-            with open(os.path.join(i18n_dir, "Index"), "wb") as f:
+            with open(i18n_file_name, "wb") as f:
                 i18n_index.dump(f, "utf-8")
 
             # Schedule this for inclusion in the Release file.
             all_series_files.add(os.path.join(component, "i18n", "Index"))
+        else:
+            if os.path.exists(i18n_file_name):
+                os.unlink(i18n_file_name)
 
     def _readIndexFileHashes(
         self, suite, file_name, subpath=None, real_file_name=None

lib/lp/archivepublisher/tests/test_publisher.py
@@ -2859,7 +2859,25 @@ class TestPublisher(TestPublisherBase):
             self.config.distsroot, "breezy-autotest", "main", "i18n"
         )
 
-        self.ubuntu["breezy-autotest"].publish_i18n_index = False
+        # Write compressed versions of a zero-length Translation-en file.
+        translation_en_index = RepositoryIndexFile(
+            os.path.join(i18n_root, "Translation-en"),
+            self.config.temproot,
+            self.ubuntutest["breezy-autotest"].index_compressors,
+        )
+        translation_en_index.close()
+
+        # First publish i18n/Index (status quo)
+        publisher._writeSuiteI18n(
+            self.ubuntutest["breezy-autotest"],
+            PackagePublishingPocket.RELEASE,
+            "main",
+            set(),
+        )
+
+        self.assertTrue(os.path.exists(os.path.join(i18n_root, "Index")))
+
+        self.ubuntutest["breezy-autotest"].publish_i18n_index = False
 
         publisher._writeSuiteI18n(
             self.ubuntutest["breezy-autotest"],
@@ -2868,6 +2886,7 @@ class TestPublisher(TestPublisherBase):
             set(),
         )
 
+        # Ensure it is removed, if previously existed
         self.assertFalse(os.path.exists(os.path.join(i18n_root, "Index")))
 
     def testReadIndexFileHashesCompression(self):