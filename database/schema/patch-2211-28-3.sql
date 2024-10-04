6ffaf4ee694243a079f351bf8df43b627e0deb89
Guruprasad Lakshmi Narayanan <guruprasad.ln@canonical.com>
2024-10-04 15:29:25 +0530

    Fix a typo in the constraint

--- database/schema/patch-2211-28-3.sql
+++ database/schema/patch-2211-28-3.sql
@@ -7,7 +7,7 @@ ALTER TABLE RockRecipe
     ADD COLUMN git_repository_url text,
     DROP CONSTRAINT consistent_git_ref,
     ADD CONSTRAINT consistent_git_ref CHECK (((git_repository IS NULL) AND (git_repository_url IS NULL)) = (git_path IS NULL)),
-    ADD CONSTRAINT (null_count(ARRAY[git_repository, octet_length(git_repository_url)]) >= 1)
+    ADD CONSTRAINT consistent_vcs CHECK (null_count(ARRAY[git_repository, octet_length(git_repository_url)]) >= 1),
     ADD CONSTRAINT valid_git_repository_url CHECK (valid_absolute_url(git_repository_url));
 
 COMMENT ON COLUMN RockRecipe.git_repository_url IS 'A URL to a Git repository with a branch containing a rock recipe.';
