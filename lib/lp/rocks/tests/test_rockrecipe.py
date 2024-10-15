72d97de404178930df1018b83e906c803bf9644e
Jürgen Gmach <juergen.gmach@canonical.com>
2024-10-15 09:43:09 +0200

    Expose IRockRecipeBuildRequest.architectures via API
    This is useful information and it will make debugging easier.

--- lib/lp/rocks/tests/test_rockrecipe.py
+++ lib/lp/rocks/tests/test_rockrecipe.py
@@ -1923,6 +1923,12 @@ class TestRockRecipeWebservice(TestCaseWithFactory):
                     "builds_collection_link": Equals(
                         build_request_url + "/builds"
                     ),
+                    "architectures": Equals(
+                        [
+                            "amd640",
+                            "risc500",
+                        ]
+                    ),
                 }
             ),
         )
