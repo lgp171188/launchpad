312e6d495bc235fb461c7b9525a2a8ae157b97b8
Ilker Emre Koc <ilkeremre.koc@canonical.com>
2025-02-27 16:12:18 +0300

    Add CRLF acceptance into FileBugDataParser
    FileBugDataParser is the only parser that works on ApportJobs i.e. jobs
    from Apport package. Originally, only the LF line-break was expected
    from Apport, and a hard-coded logic was put inside FileBugDataParser.
    The problem is, this was in violation of the RFC standards for HTTP
    request formats, which only accepts CRLF as its linebreaks. Because of
    this, an upgrade to multipart package which parsed multipart/form-data
    requests broke the apport reporting system by becoming more strict in
    its parsing by refusing to parse LF line-breaks.
    
    Thus, to upgrade multipart we need to patch apport package to only
    send CRLF line-breaks, but to patch apport package we need to
    make FileBugDataParser accept CRLF line-breaks in the first place.
    
    This commit is intended to make FileBugDataParser accept CRLF while
    retaining LF parsing for backwards compatibility.
    
    To read the input blob message without changing the cursor placement in
    the file-alias object I implemented a "reset" using a "close()" call
    followed by an "open()" call. This was because there were no "seek()"
    or "peek()" functionality in LibraryFileAlias class used in the parser
    
    In the testing module for the FileBugDataParser, I implemented a
    MockBytesIO class that inherits the built-in BytesIO class. This was
    because the tests of the parser all used BytesIO as their input
    stream/file, which doesn't have a open() method while having a close()
    method that deletes the message from the memory. Thus, I implemented
    this class so that the tests can be made with an object that acts the
    same as LibraryFileAlias in our use-case.
    
    I also added a new test text-file into the codebase to ensure we have a
    message file that uses CRLF for its line-breaks so that we can test the
    ApportJobs better.
    
    Lastly, I added some unit tests for both the parser and the
    ProcessApportJob class which uses the parser.

--- lib/lp/bugs/utilities/filebugdataparser.py
+++ lib/lp/bugs/utilities/filebugdataparser.py
@@ -32,6 +32,105 @@ class FileBugDataParser:
         self.attachments = []
         self.BUFFER_SIZE = 8192
 
+    # XXX: ilkeremrekoc 2025-02-26: The whole point of trying to find the "LF"
+    # or "CRLF" is because the Apport package (which is used to send
+    # bug-reports to Launchpad) uses LF line-breaks since its inception.
+    # This is against the RFC standards, which standardized the "CRLF" as the
+    # line-break for all HTTP requests. And because the main parser for zope
+    # (which is the "multipart" package) started enforcing this standard
+    # strictly in its newer versions, and considering we must upgrade our
+    # dependencies, we had to make sure FileBugDataParser accepts the "CRLF"
+    # as well. But we cannot accept only "CRLF" for now because the apport
+    # package for every older version of Ubuntu would still use the "LF"
+    # making it impossible to send bug-reports from older LTS versions.
+    # So, until a sufficient number or all the apport packages in future,
+    # current and past Ubuntu versions are patched to only send "CRLF" we
+    # must use this method to ensure both "LF" and "CRLF" is accepted.
+    # Once the apport package patches are done, we can revert this change,
+    # get rid of this method and its tests and simply change
+    # the "LF" expectance in the previous version into "CRLF" expectance.
+    def _findLineBreakType(self) -> bytes:
+        """Find the line-break/end_string the message is using and return it.
+
+        Assumptions:
+            - The method would be run at the start of any parsing run as this
+            method doesn't open the blob file but resets the blob-stream's
+            cursor.
+
+            - The line-break can only be LF ("\n") or CRLF ("\r\n") with
+            no CR ("\r") functionality.
+
+            - The message must have at least one line-break character when
+            parsing. An empty message or one without any line-breaks doesn't
+            count.
+
+            - The whole message must be made up of a single line-break type.
+            If more than one type is present, something will break after this
+            method.
+
+        Reads through the message until it finds an LF character ("\n") before
+        closing and reopening the file-alias/blob to reset its cursor (as the
+        file-alias/blob doesn't have a "seek" functionality). Then returns
+        whichever line-break type is used in the message.
+
+        ...
+        :raises AssertionError: If the method cannot find any LF linebreak on
+        the message stream, it raises.
+        ...
+        :return: Either byte typed CRLF (b"\r\n") or byte typed LF (b"\n")
+        :rtype: bytes
+        """
+
+        # The LF line break is assumed to be a part of the message as it is
+        # a part of both LF and CRLF.
+        lf_linebreak = b"\n"
+
+        # A temporary buffer we don't need to save outside the scope as it is
+        # only for finding the first line-break.
+        temp_buffer = b""
+
+        while lf_linebreak not in temp_buffer:
+            data = self.blob_file.read(self.BUFFER_SIZE)
+
+            # Append the data to the temp_buffer. This is to ensure any
+            # CR ("\r") that is part of a CRLF isn't deleted in a preceding
+            # buffer accidentally.
+            temp_buffer += data
+
+            if len(data) < self.BUFFER_SIZE:
+                # End of file.
+
+                if lf_linebreak not in temp_buffer:
+                    # If the linebreak isn't present, then the message must
+                    # be broken since the method must read from the start
+
+                    if (
+                        b"\r" in temp_buffer
+                    ):  # If the linebreak inside the whole message is only CR
+                        raise AssertionError(
+                            "The wrong linebreak is used. CR isn't accepted."
+                        )
+
+                    raise AssertionError(
+                        "There are no linebreaks in the blob."
+                    )
+                break
+
+        # This part is for ".seek(0)" functionality that LibraryFileAlias
+        # lacks, requiring the calls to close() -> open() back to back
+        # to reset the stream's read cursor to the start of the file.
+        self.blob_file.close()
+        self.blob_file.open()
+
+        lf_index = temp_buffer.index(lf_linebreak)
+
+        # A slice is needed even if for a single character as bytes type acts
+        # differently in slices and in single character accesses.
+        if temp_buffer[lf_index - 1 : lf_index] == b"\r":
+            return b"\r\n"
+
+        return b"\n"
+
     def _consumeBytes(self, end_string):
         """Read bytes from the message up to the end_string.
 
@@ -56,16 +155,22 @@ class FileBugDataParser:
         self._buffer = self._buffer[end_index + len(end_string) :]
         return bytes
 
-    def readHeaders(self):
-        """Read the next set of headers of the message."""
-        header_text = self._consumeBytes(b"\n\n")
+    def readHeaders(self, blob_linebreak):
+        """Read the next set of headers of the message.
+
+        :param bytes blob_linebreak: linebreak type used in the blob message
+        in bytes format e.g. b"\n", b"\r\n" """
+        header_text = self._consumeBytes(blob_linebreak + blob_linebreak)
         # Use the email package to return a dict-like object of the
         # headers, so we don't have to parse the text ourselves.
         return email.message_from_bytes(header_text)
 
-    def readLine(self):
-        """Read a line of the message."""
-        data = self._consumeBytes(b"\n")
+    def readLine(self, blob_linebreak):
+        """Read a line of the message.
+
+        :param bytes blob_linebreak: linebreak type used in the blob message
+        in bytes format e.g. b"\n", b"\r\n" """
+        data = self._consumeBytes(blob_linebreak)
         if data == b"":
             raise AssertionError("End of file reached.")
         return data
@@ -106,28 +211,31 @@ class FileBugDataParser:
         file on the file system. After using the returned data,
         removeTemporaryFiles() must be called.
         """
-        headers = self.readHeaders()
+
+        linebreak = self._findLineBreakType()
+
+        headers = self.readHeaders(linebreak)
         data = FileBugData()
         self._setDataFromHeaders(data, headers)
 
         # The headers is a Message instance.
         boundary = b"--" + six.ensure_binary(headers.get_param("boundary"))
-        line = self.readLine()
+        line = self.readLine(linebreak)
         while not line.startswith(boundary + b"--"):
             part_file = tempfile.TemporaryFile()
-            part_headers = self.readHeaders()
+            part_headers = self.readHeaders(linebreak)
             content_encoding = part_headers.get("Content-Transfer-Encoding")
             if content_encoding is not None and content_encoding != "base64":
                 raise AssertionError(
                     "Unknown encoding: %r." % content_encoding
                 )
-            line = self.readLine()
+            line = self.readLine(linebreak)
             while not line.startswith(boundary):
                 # Decode the file.
                 if content_encoding == "base64":
                     line = base64.b64decode(line)
                 part_file.write(line)
-                line = self.readLine()
+                line = self.readLine(linebreak)
             # Prepare the file for reading.
             part_file.seek(0)
             disposition = part_headers["Content-Disposition"]
